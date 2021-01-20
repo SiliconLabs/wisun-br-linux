b7343785979047e0da371cabceff1a5380637ee6
Jérôme Pouiller <jerome.pouiller@silabs.com>
2021-01-20 17:00:01 +0100

    build: add application
    Signed-off-by: Jérôme Pouiller <jerome.pouiller@silabs.com>

---
+++ main.c
@@ -0,0 +1,14 @@
+/* SPDX-License-Identifier: Apache-2.0
+ * Copyright (c) 2021, Silicon Labs
+ * Main authors:
+ *     - Jérôme Pouiller <jerome.pouiller@silabs.com>
+ */
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    printf("Hello World!\n");
+
+    return 0;
+}
+
