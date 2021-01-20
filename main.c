4cde6e6bcb9dd52258e6ef8799de61e5a79951f1
Jérôme Pouiller <jerome.pouiller@silabs.com>
2021-01-20 17:27:41 +0100

    os: add support for platform_XX_critical()
    Signed-off-by: Jérôme Pouiller <jerome.pouiller@silabs.com>

--- main.c
+++ main.c
@@ -4,11 +4,29 @@
  *     - Jérôme Pouiller <jerome.pouiller@silabs.com>
  */
 #include <stdio.h>
+#include <pthread.h>
+
+static pthread_mutex_t irqsym_lock;
 
 int main(int argc, char *argv[])
 {
     printf("Hello World!\n");
 
     return 0;
+}
+
+void platform_critical_init(void)
+{
+    pthread_mutex_init(&irqsym_lock, NULL);
+}
+
+void platform_enter_critical(void)
+{
+    pthread_mutex_lock(&irqsym_lock);
+}
+
+void platform_exit_critical(void)
+{
+    pthread_mutex_unlock(&irqsym_lock);
 }
 
