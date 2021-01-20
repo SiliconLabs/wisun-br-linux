15149d0539fcd8847fc21bc39361aee22317b262
Jérôme Pouiller <jerome.pouiller@silabs.com>
2021-01-20 18:14:47 +0100

    os: add dumb implementation for ws_pae_nvm_store_tlv_file_*()

---
+++ sli_wisun_nvm.c
@@ -0,0 +1,25 @@
+/* SPDX-License-Identifier: Apache-2.0
+ * Copyright (c) 2021, Silicon Labs
+ * Main authors:
+ *     - Jérôme Pouiller <jerome.pouiller@silabs.com>
+ */
+#include <stdio.h>
+#include <stdint.h>
+
+#include "6LoWPAN/ws/ws_pae_nvm_store.h"
+
+int8_t ws_pae_nvm_store_tlv_file_write(const char *file, nvm_tlv_t *tlv)
+{
+    printf("%s: FIXME\n", __func__);
+}
+
+int8_t ws_pae_nvm_store_tlv_file_read(const char *file, nvm_tlv_t *tlv)
+{
+    printf("%s: FIXME\n", __func__);
+}
+
+int8_t ws_pae_nvm_store_tlv_file_remove(const char *file)
+{
+    printf("%s: FIXME\n", __func__);
+}
+
