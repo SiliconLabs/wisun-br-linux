0409fbdf8da66872ed4d72cb7c80c733287a0ba8
Jérôme Pouiller <jerome.pouiller@silabs.com>
2021-01-21 11:10:26 +0100

    log: enable traces

--- main.c
+++ main.c
@@ -3,14 +3,18 @@
  * Main authors:
  *     - Jérôme Pouiller <jerome.pouiller@silabs.com>
  */
-#include <stdio.h>
 #include "hal_interrupt.h"
+#include "mbed-trace/mbed_trace.h"
+#define TRACE_GROUP  "main"
 
 int main(int argc, char *argv[])
 {
     platform_critical_init();
-    printf("Hello World!\n");
-
+    mbed_trace_init();
+    tr_debug("this is debug msg");
+    tr_info("this is info msg");
+    tr_warn("this is warning msg");
+    tr_err("this is error msg");
     return 0;
 }
 
