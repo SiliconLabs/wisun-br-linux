cc23df44b3617b3314df6a3799b814b35b933f02
Jérôme Pouiller <jerome.pouiller@silabs.com>
2021-01-25 10:20:13 +0100

    os: relocate platform_timer*()

---
+++ hal_timer.h
@@ -0,0 +1,12 @@
+/* SPDX-License-Identifier: Apache-2.0
+ * Copyright (c) 2021, Silicon Labs
+ * Main authors:
+ *     - Jérôme Pouiller <jerome.pouiller@silabs.com>
+ */
+#ifndef HAL_TIMER_H
+#define HAL_TIMER_H
+
+#include "platform/arm_hal_timer.h"
+
+#endif
+
