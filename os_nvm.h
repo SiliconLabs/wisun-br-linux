0828653f16d3988c118aa703b996483294e7aee7
Jérôme Pouiller <jerome.pouiller@silabs.com>
2021-01-25 10:27:09 +0100

    os: relocate ws_pae_nvm_store_tlv_file*()

---
+++ os_nvm.h
@@ -0,0 +1,11 @@
+/* SPDX-License-Identifier: Apache-2.0
+ * Copyright (c) 2021, Silicon Labs
+ * Main authors:
+ *     - Jérôme Pouiller <jerome.pouiller@silabs.com>
+ */
+#ifndef OS_NVM_H
+#define OS_NVM_H
+
+#include "6LoWPAN/ws/ws_pae_nvm_store.h"
+
+#endif
