63330ef61f9b47e8ec1182c8fc9116577a3c90ce
Jérôme Pouiller <jerome.pouiller@silabs.com>
2021-02-23 16:45:38 +0100

    wsmac: enable scheduled events

--- wsmac.c
+++ wsmac.c
@@ -16,6 +16,7 @@
 #include "os_timer.h"
 #include "os_types.h"
 #include "hal_interrupt.h"
+#include "eventOS_scheduler.h"
 #include "mbed-trace/mbed_trace.h"
 
 #define TRACE_GROUP  "main"
@@ -65,10 +66,12 @@ int main(int argc, char *argv[])
     struct fhss_timer_entry *fhss_timer;
     struct callback_timer *timer;
     uint64_t timer_val;
+    char event_val;
     int maxfd, ret;
     fd_set rfds;
 
     ctxt->os_ctxt = &g_os_ctxt;
+    pipe(ctxt->os_ctxt->event_fd);
     platform_critical_init();
     mbed_trace_init();
     configure(ctxt, argc, argv);
@@ -76,6 +79,8 @@ int main(int argc, char *argv[])
     for (;;) {
         maxfd = 0;
         FD_ZERO(&rfds);
+        FD_SET(ctxt->os_ctxt->event_fd[0], &rfds);
+        maxfd = max(maxfd, ctxt->os_ctxt->event_fd[0]);
         SLIST_FOR_EACH_ENTRY(ctxt->os_ctxt->timers, timer, node) {
             FD_SET(timer->fd, &rfds);
             maxfd = max(maxfd, timer->fd);
@@ -87,6 +92,10 @@ int main(int argc, char *argv[])
         ret = pselect(maxfd + 1, &rfds, NULL, NULL, NULL, NULL);
         if (ret < 0)
             FATAL(2, "pselect: %m");
+        if (FD_ISSET(ctxt->os_ctxt->event_fd[0], &rfds)) {
+            read(ctxt->os_ctxt->event_fd[0], &event_val, 1);
+            eventOS_scheduler_run_until_idle();
+        }
         SLIST_FOR_EACH_ENTRY(ctxt->os_ctxt->timers, timer, node) {
             if (FD_ISSET(timer->fd, &rfds)) {
                 read(timer->fd, &timer_val, sizeof(timer_val));
