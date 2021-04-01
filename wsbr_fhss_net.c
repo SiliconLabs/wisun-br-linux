591ef7193244e94244c22a1e69967831332d7d68
Jérôme Pouiller <jerome.pouiller@silabs.com>
2021-04-01 13:51:57 +0200

    fhss/net: add a warning if API is not implemented

--- wsbr_fhss_net.c
+++ wsbr_fhss_net.c
@@ -22,6 +22,7 @@ struct fhss_api *ns_fhss_ws_create(const struct fhss_ws_configuration *config,
 {
     // fhss_timer is filled by wsbr_configure(). We know we know we pass -1.
     BUG_ON(fhss_timer != (fhss_timer_t *)-1);
+    WARN("not implemented");
 
     // Upper layers absolutly want something != NULL
     return (struct fhss_api *)-1;
@@ -29,34 +30,40 @@ struct fhss_api *ns_fhss_ws_create(const struct fhss_ws_configuration *config,
 
 int ns_fhss_delete(struct fhss_api *fhss_api)
 {
+    WARN("not implemented");
     return 0;
 }
 
 const struct fhss_ws_configuration *ns_fhss_ws_configuration_get(const struct fhss_api *fhss_api)
 {
+    WARN("not implemented");
     return NULL;
 }
 
 int ns_fhss_ws_configuration_set(const struct fhss_api *fhss_api,
                                  const struct fhss_ws_configuration *config)
 {
+    WARN("not implemented");
     return 0;
 }
 
 int ns_fhss_ws_set_parent(const struct fhss_api *fhss_api, const uint8_t eui64[8],
                           const broadcast_timing_info_t *bc_timing_info, const bool force_synch)
 {
+    WARN("not implemented");
     return 0;
 }
 
 int ns_fhss_set_neighbor_info_fp(const struct fhss_api *fhss_api,
                                  fhss_get_neighbor_info *get_neighbor_info)
 {
+    WARN("not implemented");
     return 0;
 }
 
 int ns_fhss_ws_set_hop_count(const struct fhss_api *fhss_api, const uint8_t hop_count)
 {
+    WARN("not implemented");
     return 0;
 }
 
