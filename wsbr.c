92e00e80b5a466bdbe755e695be9667103eb3cae
Jérôme Pouiller <jerome.pouiller@silabs.com>
2021-05-18 16:49:28 +0200

    mlme/net: fix the MTU value

--- wsbr.c
+++ wsbr.c
@@ -51,7 +51,9 @@ struct wsbr_ctxt g_ctxt = {
     .mac_api.mcps_data_req_ext = wsbr_mcps_req_ext,
     .mac_api.mcps_purge_req = wsbr_mcps_purge,
 
-    .mac_api.phyMTU = MAC_IEEE_802_15_4_MAX_PHY_PACKET_SIZE,
+    // FIXME: retrieve from RCP
+    // .mac_api.phyMTU = MAC_IEEE_802_15_4G_MAX_PHY_PACKET_SIZE,
+    .mac_api.phyMTU = 2043,
 };
 
 // See warning in os_types.h
