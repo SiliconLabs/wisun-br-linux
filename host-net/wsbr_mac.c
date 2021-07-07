99c59b7c63fc3975bda45b03d70af124edd905e4
Jérôme Pouiller <jerome.pouiller@silabs.com>
2021-07-07 14:14:22 +0200

    spinel/net: use spinel_buffer for wsbr_spinel_set_data

--- host-net/wsbr_mac.c
+++ host-net/wsbr_mac.c
@@ -264,13 +264,13 @@ static void wsbr_spinel_set_eui64(struct wsbr_ctxt *ctxt, unsigned int prop, con
 
 static void wsbr_spinel_set_data(struct wsbr_ctxt *ctxt, unsigned int prop, const void *data, int data_len)
 {
-    uint8_t hdr = wsbr_get_spinel_hdr(ctxt);
-    uint8_t frame[256];
-    int frame_len;
+    struct spinel_buffer *buf = ALLOC_STACK_SPINEL_BUF(1 + 3 + 3 + 256);
 
-    frame_len = spinel_datatype_pack(frame, sizeof(frame), "CiiD", hdr, SPINEL_CMD_PROP_VALUE_SET, prop, data, data_len);
-    BUG_ON(frame_len <= 0);
-    ctxt->rcp_tx(ctxt->os_ctxt, frame, frame_len);
+    spinel_push_u8(buf, wsbr_get_spinel_hdr(ctxt));
+    spinel_push_int(buf, SPINEL_CMD_PROP_VALUE_SET);
+    spinel_push_int(buf, prop);
+    spinel_push_data(buf, data, data_len, true);
+    ctxt->rcp_tx(ctxt->os_ctxt, buf->frame, buf->cnt);
 }
 
 static void wsbr_spinel_set_cca_threshold(struct wsbr_ctxt *ctxt, unsigned int prop, const void *data, int data_len)
