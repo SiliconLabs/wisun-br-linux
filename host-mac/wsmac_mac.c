fde88a9f342e5bfaaf87345a90155780ecd6cfc3
Jérôme Pouiller <jerome.pouiller@silabs.com>
2021-07-12 18:04:34 +0200

    spinel/rcp: use spinel_buffer for wsmac_spinel_set_u8

--- host-mac/wsmac_mac.c
+++ host-mac/wsmac_mac.c
@@ -53,8 +53,8 @@ static void wsmac_spinel_set_u8(struct wsmac_ctxt *ctxt, mlme_attr_t attr, struc
         .value_size = sizeof(data),
     };
 
-    BUG_ON(spinel_remaining_size(buf) != sizeof(data));
-    spinel_datatype_unpack(spinel_ptr(buf), spinel_remaining_size(buf), "C", &data);
+    data = spinel_pop_u8(buf);
+    BUG_ON(spinel_remaining_size(buf));
     ctxt->rcp_mac_api->mlme_req(ctxt->rcp_mac_api, MLME_SET, &req);
 }
 
