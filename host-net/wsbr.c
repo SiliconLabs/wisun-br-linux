3cf14760a150d60c193b717cf6e41bc5ef92d1a4
Jérôme Pouiller <jerome.pouiller@silabs.com>
2021-08-16 16:47:14 +0200

    app/net: fix indentation

--- host-net/wsbr.c
+++ host-net/wsbr.c
@@ -454,7 +454,7 @@ int main(int argc, char *argv[])
     wsbr_rcp_get_hw_addr(ctxt);
     while (!ctxt->hw_addr_done)
         rcp_rx(ctxt);
-     memcpy(ctxt->dynamic_mac, ctxt->hw_mac, sizeof(ctxt->dynamic_mac));
+    memcpy(ctxt->dynamic_mac, ctxt->hw_mac, sizeof(ctxt->dynamic_mac));
 
     if (net_init_core())
         BUG("net_init_core");
