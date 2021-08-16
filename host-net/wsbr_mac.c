af598ef2a473751d9c6a8d5c7fd958070e5c37d4
Jérôme Pouiller <jerome.pouiller@silabs.com>
2021-08-16 16:46:38 +0200

    fixup! app/net: show version on start

--- host-net/wsbr_mac.c
+++ host-net/wsbr_mac.c
@@ -165,7 +165,7 @@ void rcp_rx(struct wsbr_ctxt *ctxt)
         version_api = spinel_pop_u32(buf);
         version_hw = spinel_pop_u32(buf);
         version_fw_str = spinel_pop_str(buf);
-        TRACE("Connect to RCP \"%s\" (%d.%d.%d), API %d.%d.%d", version_fw_str,
+        TRACE("Connected to RCP \"%s\" (%d.%d.%d), API %d.%d.%d", version_fw_str,
               FIELD_GET(0xFF000000, version_hw),
               FIELD_GET(0x00FFFF00, version_hw),
               FIELD_GET(0x000000FF, version_hw),
