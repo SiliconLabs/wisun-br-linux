e346b679fa944809adef5a13eff66f0948563f93
Jérôme Pouiller <jerome.pouiller@silabs.com>
2021-09-13 14:43:01 +0200

    app: show version using INFO() instead of DEBUG()
    Signed-off-by: Jérôme Pouiller <jerome.pouiller@silabs.com>

--- host-net/wsbr_mac.c
+++ host-net/wsbr_mac.c
@@ -158,7 +158,7 @@ void rcp_rx(struct wsbr_ctxt *ctxt)
         version_api = spinel_pop_u32(buf);
         version_hw = spinel_pop_u32(buf);
         version_fw_str = spinel_pop_str(buf);
-        DEBUG("Connected to RCP \"%s\" (%d.%d.%d), API %d.%d.%d", version_fw_str,
+        INFO("Connected to RCP \"%s\" (%d.%d.%d), API %d.%d.%d", version_fw_str,
               FIELD_GET(0xFF000000, version_hw),
               FIELD_GET(0x00FFFF00, version_hw),
               FIELD_GET(0x000000FF, version_hw),
