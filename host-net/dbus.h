052280acfef09c85cd4e57718eab0a7e600136cf
Jérôme Pouiller <jerome.pouiller@silabs.com>
2021-10-15 11:55:46 +0200

    build: libsystemd is now optionnal

--- host-net/dbus.h
+++ host-net/dbus.h
@@ -8,8 +8,28 @@
 
 struct wsbr_ctxt;
 
+#ifdef HAVE_LIBSYSTEMD
+
 void dbus_register(struct wsbr_ctxt *ctxt);
 int dbus_get_fd(struct wsbr_ctxt *ctxt);
 int dbus_process(struct wsbr_ctxt *ctxt);
+
+#else
+
+static void dbus_register(struct wsbr_ctxt *ctxt)
+{
+}
+
+static int dbus_get_fd(struct wsbr_ctxt *ctxt)
+{
+    return -1;
+}
+
+static int dbus_process(struct wsbr_ctxt *ctxt)
+{
+    return 0;
+}
+
+#endif
 
 #endif
