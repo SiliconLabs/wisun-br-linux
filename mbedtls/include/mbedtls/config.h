363179fd52ef22c94b7f61af2096e598cf066ccd
Jérôme Pouiller <jerome.pouiller@silabs.com>
2021-11-22 15:11:09 +0100

    crypto: enforcing MPI_MAX_SIZE is not justified

--- mbedtls/include/mbedtls/config.h
+++ mbedtls/include/mbedtls/config.h
@@ -3352,7 +3352,7 @@
 
 /* MPI / BIGNUM options */
 //#define MBEDTLS_MPI_WINDOW_SIZE            6 /**< Maximum windows size used. */
-#define MBEDTLS_MPI_MAX_SIZE            512
+//#define MBEDTLS_MPI_MAX_SIZE            512
 
 /* CTR_DRBG options */
 //#define MBEDTLS_CTR_DRBG_ENTROPY_LEN               48 /**< Amount of entropy used per seed by default (48 with SHA-512, 32 with SHA-256) */
