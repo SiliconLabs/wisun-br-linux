74363502b5f8cd27764d4071a7440f07fecec17b
Jérôme Pouiller <jerome.pouiller@silabs.com>
2022-03-02 13:20:11 +0100

    log: improve backtraces through libdl

--- host-common/backtrace_show.c
+++ host-common/backtrace_show.c
@@ -3,12 +3,31 @@
  * Main authors:
  *     - Jérôme Pouiller <jerome.pouiller@silabs.com>
  */
+#define _GNU_SOURCE
 #include <stdio.h>
 #include <stdbool.h>
 #include <backtrace.h>
 
+#ifdef HAVE_LIBDL
+#include <dlfcn.h>
+#endif
+
+static const char *get_symbol_with_libdl(uintptr_t pc)
+{
+#ifdef HAVE_LIBDL
+        Dl_info info;
+
+        if (dladdr((void *)pc, &info))
+            return info.dli_sname;
+#endif
+        return NULL;
+}
+
 static int backtrace_cb(void *data, uintptr_t pc, const char *filename, int lineno, const char *function)
 {
+    if (!function)
+        function = get_symbol_with_libdl(pc);
+
     if (sizeof(void *) > 4) {
         if (!function)
             printf("  %016lx ??\n", (unsigned long)pc);
