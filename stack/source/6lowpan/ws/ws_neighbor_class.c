57016a44910f4a74aa2439446a2a4370584a1280
Olivier Chanquelin <Olivier.Chanquelin@silabs.com>
2022-06-01 16:51:06 +0200

    stack: fix ws_neighbor_class_neighbor_unicast_schedule_set
    decode channel plan id from operating class for silabs interoperability.

--- stack/source/6lowpan/ws/ws_neighbor_class.c
+++ stack/source/6lowpan/ws/ws_neighbor_class.c
@@ -274,7 +274,12 @@ void ws_neighbor_class_neighbor_unicast_schedule_set(const struct protocol_inter
                 excluded_channel_ctrl = ws_us->excluded_channel_ctrl;
                 regulatory_domain = ws_us->plan.zero.regulatory_domain;
                 operating_class = ws_us->plan.zero.operating_class;
-                channel_plan_id = 255;
+                // Some chip may advertise FAN1.1 chan_plans with channel_plan=0 using this
+                // non standard extension
+                if (ws_us->plan.zero.operating_class & OPERATING_CLASS_CHAN_PLAN_ID_BIT)
+                    channel_plan_id = ws_us->plan.zero.operating_class & OPERATING_CLASS_CHAN_PLAN_ID_MASK;
+                else
+                    channel_plan_id = 255;
                 ws_neighbor->fhss_data.uc_timing_info.unicast_number_of_channels = ws_common_channel_number_calc(regulatory_domain, operating_class, channel_plan_id);
                 break;
             case 1:  // Explicit Channel Plan
