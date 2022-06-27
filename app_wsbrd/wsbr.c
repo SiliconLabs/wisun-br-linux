a80e27b75c987c1dbc37cfead9380d775c8ad0f0
Luis Thomas <luis.thomas@silabs.com>
2022-06-27 13:48:13 +0200

    wsbrd: set radius client fd before call to wsbr_fds_init()
    Fix for : WI_SUN-1568, the native socket file descriptor used in the
    radius client was previously not opened before the call to wsbr_fds_init

--- app_wsbrd/wsbr.c
+++ app_wsbrd/wsbr.c
@@ -453,6 +453,7 @@ int wsbr_main(int argc, char *argv[])
 
     if (eventOS_event_handler_create(&wsbr_tasklet, ARM_LIB_TASKLET_INIT_EVENT) < 0)
         BUG("eventOS_event_handler_create");
+    eventOS_scheduler_run_until_idle();
 
     dbus_register(ctxt);
 
