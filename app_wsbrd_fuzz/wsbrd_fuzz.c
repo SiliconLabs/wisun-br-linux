b61fac28e668a25a2d72df9a00000640cc06fc1e
Mathis Marion <mathis.marion@silabs.com>
2022-09-07 16:56:03 +0200

    fuzz: minor code clarification

--- app_wsbrd_fuzz/wsbrd_fuzz.c
+++ app_wsbrd_fuzz/wsbrd_fuzz.c
@@ -138,7 +138,7 @@ void __wrap_wsbr_spinel_replay_tun(struct spinel_buffer *buf)
 ssize_t __real_read(int fd, void *buf, size_t count);
 ssize_t __wrap_read(int fd, void *buf, size_t count)
 {
-    ssize_t ret = __real_read(fd, buf, count);
+    ssize_t size = __real_read(fd, buf, count);
     struct fuzz_ctxt *ctxt = &g_fuzz_ctxt;
 
     if (fd == g_ctxt.timerfd) {
@@ -152,12 +152,13 @@ ssize_t __wrap_read(int fd, void *buf, size_t count)
     } else if (fd == g_ctxt.tun_fd && ctxt->capture_enabled) {
         fuzz_capture_timers(ctxt);
         fuzz_capture_tun(ctxt, buf, count);
-    } else if (fd == g_ctxt.os_ctxt->data_fd && !ret && ctxt->replay_i < ctxt->replay_count) {
+    } else if (fd == g_ctxt.os_ctxt->data_fd && !size && ctxt->replay_i < ctxt->replay_count) {
+        // Read from the next replay file
         g_ctxt.os_ctxt->data_fd = ctxt->replay_fds[ctxt->replay_i++];
         return __real_read(g_ctxt.os_ctxt->data_fd, buf, count);
     }
 
-    return ret;
+    return size;
 }
 
 ssize_t __real_write(int fd, const void *buf, size_t count);
