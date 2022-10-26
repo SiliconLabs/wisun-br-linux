b3cb74b62429b3219002892487bb8cfa1d216718
Jérôme Pouiller <jerome.pouiller@silabs.com>
2022-10-26 12:46:23 +0200

    stack: drop unused ws_pae_time functions
    See also WI_SUN-1322

--- stack/source/6lowpan/ws/ws_pae_time.h
+++ stack/source/6lowpan/ws/ws_pae_time.h
@@ -20,74 +20,8 @@
 #include <stdint.h>
 #include <stdbool.h>
 
-/*
- * Port access entity time functions.
- *
- */
-
-typedef enum {
-    TIME_FORMAT_SECONDS = 0,
-    TIME_FORMAT_MINUTES,
-    TIME_FORMAT_HOURS,
-    TIME_FORMAT_DAYS,
-} time_format_e;
-
-#define STIME_TIME_BITS             14
-#define STIME_TIME_MAX              0x3FFF
-#define STIME_TIME_MASK             STIME_TIME_MAX
-
-#define STIME_FORMAT_GET(stime) (stime >> STIME_TIME_BITS)
-#define STIME_TIME_GET(stime) (stime & STIME_TIME_MASK)
-
 // Maximum difference in stored and indicated system time
 #define SYSTEM_TIME_MAXIMUM_DIFF   (60 * 60 * 24 * 30) // One month
-
-/**
- * ws_pae_time_to_short_convert convert time to short format
- *
- * \param time time in seconds to convert
- *
- * \return < 0 failure
- * \return >= 0 success
- *
- */
-uint16_t ws_pae_time_to_short_convert(uint32_t time);
-
-/**
- * ws_pae_time_from_short_convert convert short time to time format
- *
- * \param short_time short_time to convert
- *
- * \return time in seconds
- *
- */
-uint32_t ws_pae_time_from_short_convert(uint16_t short_time);
-
-/**
- * ws_pae_time_from_short_time_compare compare two times in short format
- *
- * \param short_time1 time 1 to compare
- * \param short_time1 time 2 to compare
- *
- * \return true times are equal
- * \return false times are not equal
- *
- */
-bool ws_pae_time_from_short_time_compare(uint16_t short_time1, uint16_t short_time2);
-
-/**
- * ws_pae_time_diff_calc calculates difference between two times
- *
- * \param curr_time current time
- * \param comp_time time which is compared
- * \param time_diff returns time difference
- * \param future compared time should in future
- *
- * \return < 0 failure
- * \return >= 0 success
- *
- */
-int8_t ws_pae_time_diff_calc(uint64_t curr_time, uint64_t comp_time, uint32_t *time_diff, bool future);
 
 /**
  * ws_pae_time_old_or_new_select selected old or new time (based on difference)
