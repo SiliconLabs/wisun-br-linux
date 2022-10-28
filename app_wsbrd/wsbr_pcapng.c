093cc7b14d26e1d753cf6099ca4c69c8adf1f244
Mathis Marion <mathis.marion@silabs.com>
2022-10-28 15:46:26 +0200

    wsbrd: write pcapng header blocks

--- app_wsbrd/wsbr_pcapng.c
+++ app_wsbrd/wsbr_pcapng.c
@@ -1,10 +1,41 @@
 #define _DEFAULT_SOURCE
 #include <sys/stat.h>
+#include <unistd.h>
 #include <fcntl.h>
 #include <errno.h>
 
 #include "common/log.h"
+#include "common/pcapng.h"
 #include "wsbr.h"
+
+static void wsbr_pcapng_write(struct wsbr_ctxt *ctxt, const struct pcapng_buf *buf)
+{
+    int ret;
+
+    ret = write(ctxt->pcapng_fd, buf->buf, buf->cnt);
+    if (ret < 0 && !(ret == -EAGAIN && ctxt->pcapng_type == S_IFIFO))
+        FATAL(2, "write pcapng: %m");
+}
+
+static void wsbr_pcapng_write_start(struct wsbr_ctxt *ctxt)
+{
+    static const struct pcapng_shb shb = {
+        .version_maj = 1,
+        .version_min = 0,
+        .section_len = -1, // unknown section length
+    };
+    static const struct pcapng_idb idb = {
+        .link_type = LINKTYPE_IEEE802_15_4_NOFCS,
+        .snap_len = 0, // no packet size restriction
+    };
+    struct pcapng_buf *buf = ALLOC_STACK_PCAPNG_BUF(
+        PCAPNG_SHB_SIZE_MIN + PCAPNG_IDB_SIZE_MIN
+    );
+
+    pcapng_write_shb(buf, &shb);
+    pcapng_write_idb(buf, &idb);
+    wsbr_pcapng_write(ctxt, buf);
+}
 
 void wsbr_pcapng_init(struct wsbr_ctxt *ctxt)
 {
@@ -32,4 +63,6 @@ void wsbr_pcapng_init(struct wsbr_ctxt *ctxt)
         ctxt->pcapng_fd = open(ctxt->config.pcap_file, O_WRONLY | O_CREAT | O_TRUNC, 0666);
         FATAL_ON(ctxt->pcapng_fd < 0, 2, "open %s: %m", ctxt->config.pcap_file);
     }
+
+    wsbr_pcapng_write_start(ctxt);
 }
