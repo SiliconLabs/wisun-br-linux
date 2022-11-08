d535d004d2cfa6df0d37e744367c652e56fdae9c
Mathis Marion <mathis.marion@silabs.com>
2022-11-08 10:18:50 +0100

    common: introduce memrcpy()

--- common/string_extra.h
+++ common/string_extra.h
@@ -26,4 +26,14 @@ static inline int memzcmp(const void *src, size_t size)
 	return memcmp(buf, buf + 1, size - 1);
 }
 
+static inline void *memrcpy(void *dst, const void *src, size_t size)
+{
+    const uint8_t *src8 = src;
+    uint8_t *dst8 = dst;
+
+    for (int i = 0; i < size; i++)
+        dst8[i] = src8[size - i - 1];
+    return dst;
+}
+
 #endif
