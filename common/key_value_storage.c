8fe66cabb49f4a4f3209bc68444b3f326fa9d4fe
Mathis Marion <mathis.marion@silabs.com>
2022-12-20 11:05:17 +0100

    utils: introduce storage_delete()
    See also WI_SUN-2115

--- common/key_value_storage.c
+++ common/key_value_storage.c
@@ -16,6 +16,7 @@
 #include <unistd.h>
 #include <libgen.h>
 #include <errno.h>
+#include <glob.h>
 #include "common/log.h"
 
 #include "key_value_storage.h"
@@ -115,4 +116,25 @@ int storage_parse_line(struct storage_parse_info *info)
     if (sscanf(info->key, "%*[^[][%u]", &info->key_array_index) != 1)
         info->key_array_index = UINT_MAX;
     return 0;
+}
+
+void storage_delete()
+{
+    char filename[PATH_MAX];
+    glob_t globbuf;
+    int ret;
+
+    if (!g_storage_prefix)
+        return;
+    snprintf(filename, sizeof(filename), "%s*", g_storage_prefix);
+    ret = glob(filename, 0, NULL, &globbuf);
+    if (ret) {
+        WARN_ON(ret != GLOB_NOMATCH, "glob %s returned an error", filename);
+        return;
+    }
+    for (int i = 0; globbuf.gl_pathv[i]; i++) {
+        ret = unlink(globbuf.gl_pathv[i]);
+        WARN_ON(ret < 0, "unlink %s: %m", globbuf.gl_pathv[i]);
+    }
+    globfree(&globbuf);
 }
