117d9831df04902a33768f8dce9467ff2aa07442
Mathis Marion <mathis.marion@silabs.com>
2023-01-05 15:25:22 +0100

    wsrouter: fix bootstrap_ffn missing header

--- stack/source/6lowpan/ws/ws_bootstrap_ffn.c
+++ stack/source/6lowpan/ws/ws_bootstrap_ffn.c
@@ -61,6 +61,7 @@
 
 #include "6lowpan/ws/ws_bbr_api_internal.h"
 #include "6lowpan/ws/ws_bootstrap.h"
+#include "6lowpan/ws/ws_bootstrap_ffn.h"
 #include "6lowpan/ws/ws_cfg_settings.h"
 #include "6lowpan/ws/ws_common_defines.h"
 #include "6lowpan/ws/ws_common.h"
