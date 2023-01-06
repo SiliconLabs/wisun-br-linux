981390e2bbca30f4643fe03f063749b08daf2b96
Mathis Marion <mathis.marion@silabs.com>
2023-01-06 15:39:40 +0100

    ns3: seed RNG with simulation ID
    See also SADDLE-444

--- app_wsbrd_ns3/rand.cpp
+++ app_wsbrd_ns3/rand.cpp
@@ -1,11 +1,20 @@
 #include <sys/types.h>
 
 #include <ns3/random-variable-stream.h>
+#include <ns3/rng-seed-manager.h>
+
+#include "wsbrd_ns3.hpp"
 
 extern "C" ssize_t __wrap_getrandom(void *buf, size_t buflen, unsigned int flags)
 {
+    static bool init = false;
     static ns3::Ptr<ns3::UniformRandomVariable> rand_source =
         ns3::CreateObject<ns3::UniformRandomVariable>();
+
+    if (!init) {
+        rand_source->SetStream(ns3::RngSeedManager::GetSeed() + g_simulation_id);
+        init = true;
+    }
 
     for (size_t i = 0; i < buflen; i++)
         ((uint8_t *)buf)[i] = rand_source->GetInteger(0, 255);
