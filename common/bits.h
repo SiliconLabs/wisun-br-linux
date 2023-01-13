00ff4ca38b2149c83bf686d18211e57469a558b7
Jérôme Pouiller <jerome.pouiller@silabs.com>
2023-01-13 20:24:33 +0100

    doc: add documentation to bits.h

--- common/bits.h
+++ common/bits.h
@@ -12,19 +12,51 @@
  */
 #ifndef BITS_H
 #define BITS_H
-
 #include <stdbool.h>
 #include <stddef.h>
 #include <stdint.h>
 
+/*
+ * Provide in fact two different APIs:
+ *   - FIELD_*()
+ *   - bit*()
+ */
+
+/*
+ * FIELD_GET() and FIELD_PREP() are macros make access to bit fields easy. The
+ * caller just provide the mask. The macros automatically compute the necessary
+ * shift. This makes the definition of bit fields more terse.
+ *
+ * There is no FIELD_SET() macro (a macro that change it arguments lead to
+ * questions during when developer read the code). The proposed way to set bits
+ * are:
+ *
+ *   var &= ~MASK;
+ *   var |= FIELD_PREP(val, mask);
+ *
+ * or (more usual finally):
+ *
+ *   var = 0;
+ *   var |= FIELD_PREP(val1, mask2);
+ *   var |= FIELD_PREP(val2, mask2);
+ */
 #define __CTZ(value) __builtin_ctz(value)
 #define FIELD_GET(mask, reg) (((reg) & (mask)) >> __CTZ(mask))
 #define FIELD_PREP(mask, val) (((val) << __CTZ(mask)) & (mask))
 
+/*
+ * Functions bit*() allow to work on array of bytes. So they can work on bits
+ * > 64. In these functions, start, end, i, etc... are always expressed as a
+ * number of bits (1 bytes == 8 bits).
+ *
+ * No boundary check are not done by these functions. Some of them take
+ * the number of bit as argument, but only because they need it.
+ * bitcpy() and bitcmp() mimics the behavior of memcpy() and memcmp().
+ */
 void *bitfill(void *dst, bool val, size_t start, size_t end);
-void *bitcpy(void *dst, const void *src, size_t len);
-void *bitcpy0(void *dst, const void *src, size_t len);
-int bitcmp(const void *s1, const void *s2, size_t len);
+void *bitcpy(void *dst, const void *src, size_t nbits);
+void *bitcpy0(void *dst, const void *src, size_t nbits);
+int bitcmp(const void *s1, const void *s2, size_t nbits);
 bool bitcmp0(const void *s1, size_t len);
 void bitand(uint8_t *dst, const uint8_t *src, int nbits);
 int bitcnt(const uint8_t *bits, int nbits);
@@ -32,7 +64,12 @@ bool bittest(const uint8_t *bits, int i);
 void bitset(uint8_t *bits, int i);
 void bitclr(uint8_t *bits, int i);
 
-// Operations with reversed bit order
+/*
+ * Functions bitr*() act as their bit*() counterparts, but reversing the bit
+ * order. So MSB is bit number 0 in these functions. They are used to work with
+ * channel mask in the WI-SUN IEs. We introduce the type uint_rev8_t to easily
+ * identify these uncommon arrays of bits.
+ */
 typedef uint8_t uint_rev8_t;
 bool bitrtest(const uint_rev8_t *bits, int i);
 void bitrset(uint_rev8_t *bits, int i);
