2aa399b9d4c5a4eaf8fe9c94fa738a2b2403bf6e
Jérôme Pouiller <jerome.pouiller@silabs.com>
2023-03-21 15:08:55 +0100

    stash: fix headers includes in wsbr_pcapng.h

--- app_wsbrd/wsbr_pcapng.h
+++ app_wsbrd/wsbr_pcapng.h
@@ -1,10 +1,11 @@
 #ifndef WSBR_PCAPNG_H
 #define WSBR_PCAPNG_H
 
-typedef struct mcps_data_ie_list mcps_data_ie_list_t;
-typedef struct mcps_data_ind mcps_data_ind_t;
+struct wsbr_ctxt;
+struct mcps_data_ind;
+struct mcps_data_ie_list;
 
 void wsbr_pcapng_init(struct wsbr_ctxt *ctxt);
-void wsbr_pcapng_write_frame(struct wsbr_ctxt *ctxt, mcps_data_ind_t *ind, mcps_data_ie_list_t *ie);
+void wsbr_pcapng_write_frame(struct wsbr_ctxt *ctxt, struct mcps_data_ind *ind, struct mcps_data_ie_list *ie);
 
 #endif
