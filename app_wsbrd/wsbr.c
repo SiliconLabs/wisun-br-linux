c9e39d45d031110f51300dd64cf02ef1f13064df
Mathis Marion <mathis.marion@silabs.com>
2023-07-05 16:55:15 +0200

    fixup! stack: rework mac_helper_mac_mlme_filter_add_long()

--- app_wsbrd/wsbr.c
+++ app_wsbrd/wsbr.c
@@ -132,7 +132,7 @@ static void ws_enable_mac_filtering(struct wsbr_ctxt *ctxt)
     for (i = 0; i < ctxt->config.ws_allowed_mac_address_count; i++)
         rcp_add_mac_filter_entry(ctxt->config.ws_allowed_mac_addresses[i], true);
     for (i = 0; i < ctxt->config.ws_denied_mac_address_count; i++)
-        rcp_add_mac_filter_entry(ctxt->config.ws_allowed_mac_addresses[i], false);
+        rcp_add_mac_filter_entry(ctxt->config.ws_denied_mac_addresses[i], false);
 }
 
 static int wsbr_configure_ws_sect_time(struct wsbr_ctxt *ctxt)
