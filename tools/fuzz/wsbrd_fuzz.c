ecc2faf19fbb9ed80cf796bba4c1c6ba47c64d3a
Mathis Marion <mathis.marion@silabs.com>
2023-08-10 17:28:38 +0200

    fuzz: fix capture of TUN packets
    The rx buffer size was used instead of the actual packet size, causing
    sometimes packets to drop with "Packet too big".
    
    To still replay corrupted files, the following diff can be applied:
    
    diff --git a/app_wsbrd/tun.c b/app_wsbrd/tun.c
    index 8188bbeed7..de59ebc8f2 100644
    --- a/app_wsbrd/tun.c
    +++ b/app_wsbrd/tun.c
    @@ -432,9 +432,9 @@ void wsbr_tun_read(struct wsbr_ctxt *ctxt)
         if (!buf_6lowpan)
             FATAL(1,"could not allocate tun buffer_t");
         buf_6lowpan->interface = cur;
    -    buffer_data_add(buf_6lowpan, iobuf.data, iobuf.data_size);
    
         buf_6lowpan->payload_length    = iobuf_pop_be16(&iobuf);
    +    buffer_data_add(buf_6lowpan, iobuf.data, buf_6lowpan->payload_length + 40);
         nxthdr                         = iobuf_pop_u8(&iobuf);
         buf_6lowpan->options.hop_limit = iobuf_pop_u8(&iobuf);
         buf_6lowpan->src_sa.addr_type = ADDR_IPV6;

--- tools/fuzz/wsbrd_fuzz.c
+++ tools/fuzz/wsbrd_fuzz.c
@@ -129,7 +129,7 @@ ssize_t __wrap_read(int fd, void *buf, size_t count)
         }
     } else if (fd == g_ctxt.tun_fd && ctxt->capture_fd >= 0) {
         fuzz_capture_timers(ctxt);
-        fuzz_capture_interface(ctxt, IF_TUN, ADDR_UNSPECIFIED, 0, buf, count);
+        fuzz_capture_interface(ctxt, IF_TUN, ADDR_UNSPECIFIED, 0, buf, size);
     } else if (fd == g_ctxt.os_ctxt->data_fd && !size && ctxt->replay_i < ctxt->replay_count) {
         // Read from the next replay file
         g_ctxt.os_ctxt->data_fd = ctxt->replay_fds[ctxt->replay_i++];
