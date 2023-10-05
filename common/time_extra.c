7bf0d22181044bf4e1cfc63c9299ccb80cb8c0e5
Kylian Balan <kylian.balan@silabs.com>
2023-10-05 16:25:21 +0200

    common: introduce time api

---
+++ common/time_extra.c
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2023 Silicon Laboratories Inc. (www.silabs.com)
+ *
+ * The licensor of this software is Silicon Laboratories Inc. Your use of this
+ * software is governed by the terms of the Silicon Labs Master Software License
+ * Agreement (MSLA) available at [1].  This software is distributed to you in
+ * Object Code format and/or Source Code format and is governed by the sections
+ * of the MSLA applicable to Object Code, Source Code and Modified Open Source
+ * Code. By using this software, you agree to the terms of the MSLA.
+ *
+ * [1]: https://www.silabs.com/about-us/legal/master-software-license-agreement
+ */
+
+#include <time.h>
+
+time_t time_current(clockid_t clockid)
+{
+    struct timespec tp;
+
+    clock_gettime(clockid, &tp);
+    return tp.tv_sec;
+}
+
+time_t time_get_elapsed(clockid_t clockid, time_t start)
+{
+    struct timespec tp;
+
+    clock_gettime(clockid, &tp);
+    return tp.tv_sec - start;
+}
---
+++ common/time_extra.h
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2023 Silicon Laboratories Inc. (www.silabs.com)
+ *
+ * The licensor of this software is Silicon Laboratories Inc. Your use of this
+ * software is governed by the terms of the Silicon Labs Master Software License
+ * Agreement (MSLA) available at [1].  This software is distributed to you in
+ * Object Code format and/or Source Code format and is governed by the sections
+ * of the MSLA applicable to Object Code, Source Code and Modified Open Source
+ * Code. By using this software, you agree to the terms of the MSLA.
+ *
+ * [1]: https://www.silabs.com/about-us/legal/master-software-license-agreement
+ */
+
+#ifndef TIME_EXTRA_H
+#define TIME_EXTRA_H
+
+#include <time.h>
+
+time_t time_current(clockid_t clockid);
+
+time_t time_get_elapsed(clockid_t clockid, time_t start);
+
+#endif /* TIME_EXTRA_H */
