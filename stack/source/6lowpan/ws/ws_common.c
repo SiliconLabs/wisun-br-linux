0f297c9fe5ddbe7fca6175f919c4cd6cbc8b43a5
Kylian Balan <kylian.balan@silabs.com>
2023-11-06 16:19:04 +0100

    stack: drop useless ws_common_neighbor_remove()
    Reviewed-by: Jérôme Pouiller <jerome.pouiller@silabs.com>

--- stack/source/6lowpan/ws/ws_common.c
+++ stack/source/6lowpan/ws/ws_common.c
@@ -211,12 +211,6 @@ void ws_common_aro_failure(struct net_if *cur, const uint8_t *ll_address)
     ws_bootstrap_aro_failure(cur, ll_address);
 }
 
-void ws_common_neighbor_remove(struct net_if *cur, const uint8_t *ll_address)
-{
-    tr_debug("neighbor remove %s", tr_ipv6(ll_address));
-    ws_bootstrap_neighbor_remove(cur, ll_address);
-}
-
 uint8_t ws_common_temporary_entry_size(uint8_t mac_table_size)
 {
     if (mac_table_size >= 128) {
--- stack/source/6lowpan/ws/ws_common.h
+++ stack/source/6lowpan/ws/ws_common.h
@@ -157,8 +157,6 @@ void ws_common_black_list_neighbour(const uint8_t *ll_address, uint8_t nd_status
 
 void ws_common_aro_failure(struct net_if *cur, const uint8_t *ll_address);
 
-void ws_common_neighbor_remove(struct net_if *cur, const uint8_t *ll_address);
-
 uint8_t ws_common_allow_child_registration(struct net_if *cur, const uint8_t *eui64, uint16_t aro_timeout);
 
 bool ws_common_negative_aro_mark(struct net_if *interface, const uint8_t *eui64);
