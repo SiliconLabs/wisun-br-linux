2c7cce2c5eb2c5aa7da1f86591fce0d1c52a0f18
Mathis Marion <mathis.marion@silabs.com>
2023-11-22 14:42:49 +0100

    stack: drop forward function declarations in ws_bootstrap.c
    Reviewed-by: Jérôme Pouiller <jerome.pouiller@silabs.com>

--- stack/source/6lowpan/ws/ws_bootstrap.c
+++ stack/source/6lowpan/ws/ws_bootstrap.c
@@ -72,21 +72,6 @@
 
 #define TRACE_GROUP "wsbs"
 
-static void ws_bootstrap_event_handler(struct event_payload *event);
-static int8_t ws_bootstrap_event_trig(ws_bootstrap_event_type_e event_type, int8_t interface_id, void *event_data);
-static void ws_bootstrap_nw_key_set(struct net_if *cur, uint8_t operation, uint8_t index, uint8_t *key);
-static void ws_bootstrap_nw_key_clear(struct net_if *cur, uint8_t slot);
-static void ws_bootstrap_nw_key_index_set(struct net_if *cur, uint8_t index);
-static void ws_bootstrap_nw_frame_counter_set(struct net_if *cur, uint32_t counter, uint8_t slot);
-static void ws_bootstrap_nw_frame_counter_read(struct net_if *cur, uint8_t slot);
-static void ws_bootstrap_nw_info_updated(struct net_if *interface_ptr, uint16_t pan_id, uint16_t pan_version, uint16_t lfn_version);
-static bool ws_bootstrap_eapol_congestion_get(struct net_if *interface_ptr, uint16_t active_supp);
-static void ws_bootstrap_pan_version_increment(struct net_if *cur);
-static void ws_bootstrap_lfn_version_increment(struct net_if *cur);
-static ws_nud_table_entry_t *ws_nud_entry_discover(struct net_if *cur, void *neighbor);
-static void ws_nud_entry_remove(struct net_if *cur, mac_neighbor_table_entry_t *entry_ptr);
-static bool ws_neighbor_entry_nud_notify(mac_neighbor_table_entry_t *entry_ptr, void *user_data);
-
 static mac_neighbor_table_entry_t *ws_bootstrap_mac_neighbor_allocate(struct net_if *interface, const uint8_t *mac64, uint8_t role)
 {
     mac_neighbor_table_entry_t *neighbor = mac_neighbor_table_entry_allocate(interface->mac_parameters.mac_neighbor_table, mac64);
@@ -171,6 +156,17 @@ static void ws_bootstrap_address_notification_cb(struct net_if *interface, const
             interface->bootstrap_state_machine_cnt = 10; //Re trigger state check
 }
 
+static void ws_bootstrap_event_handler(struct event_payload *event)
+{
+    struct net_if *cur;
+    cur = protocol_stack_interface_info_get_by_bootstrap_id(event->receiver);
+    if (!cur) {
+        return;
+    }
+
+    ws_bootstrap_6lbr_event_handler(cur, event);
+}
+
 static int ws_bootstrap_tasklet_init(struct net_if *cur)
 {
     if (cur->bootStrapId < 0)
@@ -228,6 +224,16 @@ static ws_nud_table_entry_t *ws_nud_entry_get_free(struct net_if *cur)
     return entry;
 }
 
+static ws_nud_table_entry_t *ws_nud_entry_discover(struct net_if *cur, void *neighbor)
+{
+    ns_list_foreach(ws_nud_table_entry_t, entry, &cur->ws_info.active_nud_process) {
+        if (entry->neighbor_info == neighbor) {
+            return entry;
+        }
+    }
+    return NULL;
+}
+
 void ws_nud_entry_remove_active(struct net_if *cur, void *neighbor)
 {
     ws_nud_table_entry_t *entry = ws_nud_entry_discover(cur, neighbor);
@@ -242,16 +248,6 @@ void ws_nud_entry_remove_active(struct net_if *cur, void *neighbor)
 
         mac_neighbor_table_neighbor_connected(cur->mac_parameters.mac_neighbor_table, mac_neighbor);
     }
-}
-
-static ws_nud_table_entry_t *ws_nud_entry_discover(struct net_if *cur, void *neighbor)
-{
-    ns_list_foreach(ws_nud_table_entry_t, entry, &cur->ws_info.active_nud_process) {
-        if (entry->neighbor_info == neighbor) {
-            return entry;
-        }
-    }
-    return NULL;
 }
 
 static void ws_nud_state_clean(struct net_if *cur, ws_nud_table_entry_t *entry)
@@ -843,6 +839,160 @@ static bool ws_neighbor_entry_nud_notify(mac_neighbor_table_entry_t *entry_ptr, 
     return true;
 }
 
+static void ws_bootstrap_pan_version_increment(struct net_if *cur)
+{
+    (void)cur;
+    ws_bbr_pan_version_increase(cur);
+}
+
+static void ws_bootstrap_lfn_version_increment(struct net_if *cur)
+{
+    (void)cur;
+    ws_bbr_lfn_version_increase(cur);
+}
+
+static void ws_bootstrap_nw_key_set(struct net_if *cur, uint8_t slot, uint8_t index, uint8_t *key)
+{
+    // Firmware API < 0.15 crashes if slots > 3 are accessed
+    if (!cur->ws_info.enable_lfn && slot > 3)
+        return;
+    mac_helper_security_key_to_descriptor_set(cur, key, index + 1, slot);
+}
+
+static void ws_bootstrap_nw_key_clear(struct net_if *cur, uint8_t slot)
+{
+    // Firmware API < 0.15 crashes if slots > 3 are accessed
+    if (!cur->ws_info.enable_lfn && slot > 3)
+        return;
+    mac_helper_security_key_descriptor_clear(cur, slot);
+}
+
+static void ws_bootstrap_nw_key_index_set(struct net_if *cur, uint8_t index)
+{
+    if (cur->mac_parameters.mac_default_ffn_key_index != 0 &&
+        cur->mac_parameters.mac_default_ffn_key_index != index + 1 &&
+        index < 4) {
+        /* Update the active key in the PAN Configs */
+        tr_info("New Pending key Request %u", index);
+        cur->ws_info.pending_key_index_info.state = PENDING_KEY_INDEX_ADVERTISMENT;
+        cur->ws_info.pending_key_index_info.index = index;
+        return;
+    }
+    if (cur->mac_parameters.mac_default_lfn_key_index != 0 &&
+        cur->mac_parameters.mac_default_lfn_key_index != index + 1 &&
+        index >= 4 && index < 7)
+        // Notify LFNs that a new LGTK has been activated.
+        ws_mngt_lpc_pae_cb(cur);
+
+    /* Deprecated: Unused by the RCP. */
+    if (index < 4)
+        cur->mac_parameters.mac_default_ffn_key_index = index + 1;
+    else if (index >= 4 && index < 7)
+        cur->mac_parameters.mac_default_lfn_key_index = index + 1;
+}
+
+static void ws_bootstrap_nw_frame_counter_set(struct net_if *cur, uint32_t counter, uint8_t slot)
+{
+    rcp_legacy_set_frame_counter(slot, counter);
+}
+
+static void ws_bootstrap_nw_frame_counter_read(struct net_if *cur, uint8_t slot)
+{
+    // Read frame counter
+    mac_helper_key_link_frame_counter_read(cur->id, slot);
+}
+
+static void ws_bootstrap_nw_info_updated(struct net_if *cur, uint16_t pan_id, uint16_t pan_version, uint16_t lfn_version)
+{
+    /* For border router, the PAE controller reads PAN ID, PAN version and network name from storage.
+     * If they are set, takes them into use here.
+     */
+    // Get network name
+    ws_gen_cfg_t gen_cfg;
+    if (ws_cfg_gen_get(&gen_cfg) < 0) {
+        return;
+    }
+
+    // If PAN ID has not been set, set it
+    if (cur->ws_info.network_pan_id == 0xffff) {
+        cur->ws_info.network_pan_id = pan_id;
+        // Sets PAN version
+        cur->ws_info.pan_information.pan_version = pan_version;
+        cur->ws_info.pan_information.pan_version_set = true;
+        cur->ws_info.pan_information.lfn_version = lfn_version;
+        cur->ws_info.pan_information.lfn_version_set = true;
+    }
+
+    // Stores the settings
+    ws_cfg_gen_set(cur, &gen_cfg, 0);
+}
+
+static bool ws_bootstrap_eapol_congestion_get(struct net_if *cur, uint16_t active_supp)
+{
+    if (cur == NULL || cur->random_early_detection == NULL || cur->llc_random_early_detection == NULL || cur->llc_eapol_random_early_detection == NULL) {
+        return false;
+    }
+
+    bool return_value = false;
+    static struct red_info *red_info = NULL;
+    uint16_t adaptation_average = 0;
+    uint16_t llc_average = 0;
+    uint16_t llc_eapol_average = 0;
+    uint16_t average_sum = 0;
+    uint8_t active_max = 0;
+    uint32_t heap_size = UINT32_MAX;
+
+    /*
+      * For different memory sizes the max simultaneous authentications will be
+      * 32k:    (32k / 50k) * 2 + 1 = 1
+      * 65k:    (65k / 50k) * 2 + 1 = 3
+      * 250k:   (250k / 50k) * 2 + 1 = 11
+      * 1000k:  (1000k / 50k) * 2 + 1 = 41
+      * 2000k:  (2000k / 50k) * 2 + 1 = 50 (upper limit)
+      */
+    active_max = (heap_size / 50000) * 2 + 1;
+    if (active_max > 50) {
+        active_max = 50;
+    }
+
+    // Read the values for adaptation and LLC queues
+    adaptation_average = random_early_detection_aq_read(cur->random_early_detection);
+    llc_average = random_early_detection_aq_read(cur->llc_random_early_detection);
+    llc_eapol_average  = random_early_detection_aq_read(cur->llc_eapol_random_early_detection);
+    // Calculate combined average
+    average_sum = adaptation_average + llc_average + llc_eapol_average;
+
+    // Maximum for active supplicants based on memory reached, fail
+    if (active_supp >= active_max) {
+        return_value = true;
+        goto congestion_get_end;
+    }
+
+    // Always allow at least five negotiations (if memory does not limit)
+    if (active_supp < 5) {
+        goto congestion_get_end;
+    }
+
+    if (red_info == NULL) {
+        red_info = random_early_detection_create(
+                       cur->ws_info.cfg->sec_prot.max_simult_sec_neg_tx_queue_min,
+                       cur->ws_info.cfg->sec_prot.max_simult_sec_neg_tx_queue_max,
+                       100, RED_AVERAGE_WEIGHT_DISABLED);
+    }
+    if (red_info == NULL) {
+        goto congestion_get_end;
+    }
+
+    // Check drop probability
+    average_sum = random_early_detection_aq_calc(red_info, average_sum);
+    return_value = random_early_detection_congestion_check(red_info);
+
+congestion_get_end:
+    tr_info("Active supplicant limit, active: %i max: %i summed averageQ: %i adapt averageQ: %i LLC averageQ: %i LLC EAPOL averageQ: %i drop: %s", active_supp, active_max, average_sum, adaptation_average, llc_average, llc_eapol_average, return_value ? "T" : "F");
+
+    return return_value;
+}
+
 int ws_bootstrap_init(int8_t interface_id)
 {
     struct net_if *cur = protocol_stack_interface_info_get_by_id(interface_id);
@@ -1122,160 +1272,6 @@ void ws_bootstrap_advertise_start(struct net_if *cur)
     trickle_start(&cur->ws_info.mngt.trickle_pc, "CFG", &cur->ws_info.mngt.trickle_params);
 }
 
-static void ws_bootstrap_pan_version_increment(struct net_if *cur)
-{
-    (void)cur;
-    ws_bbr_pan_version_increase(cur);
-}
-
-static void ws_bootstrap_lfn_version_increment(struct net_if *cur)
-{
-    (void)cur;
-    ws_bbr_lfn_version_increase(cur);
-}
-
-static void ws_bootstrap_nw_key_set(struct net_if *cur, uint8_t slot, uint8_t index, uint8_t *key)
-{
-    // Firmware API < 0.15 crashes if slots > 3 are accessed
-    if (!cur->ws_info.enable_lfn && slot > 3)
-        return;
-    mac_helper_security_key_to_descriptor_set(cur, key, index + 1, slot);
-}
-
-static void ws_bootstrap_nw_key_clear(struct net_if *cur, uint8_t slot)
-{
-    // Firmware API < 0.15 crashes if slots > 3 are accessed
-    if (!cur->ws_info.enable_lfn && slot > 3)
-        return;
-    mac_helper_security_key_descriptor_clear(cur, slot);
-}
-
-static void ws_bootstrap_nw_key_index_set(struct net_if *cur, uint8_t index)
-{
-    if (cur->mac_parameters.mac_default_ffn_key_index != 0 &&
-        cur->mac_parameters.mac_default_ffn_key_index != index + 1 &&
-        index < 4) {
-        /* Update the active key in the PAN Configs */
-        tr_info("New Pending key Request %u", index);
-        cur->ws_info.pending_key_index_info.state = PENDING_KEY_INDEX_ADVERTISMENT;
-        cur->ws_info.pending_key_index_info.index = index;
-        return;
-    }
-    if (cur->mac_parameters.mac_default_lfn_key_index != 0 &&
-        cur->mac_parameters.mac_default_lfn_key_index != index + 1 &&
-        index >= 4 && index < 7)
-        // Notify LFNs that a new LGTK has been activated.
-        ws_mngt_lpc_pae_cb(cur);
-
-    /* Deprecated: Unused by the RCP. */
-    if (index < 4)
-        cur->mac_parameters.mac_default_ffn_key_index = index + 1;
-    else if (index >= 4 && index < 7)
-        cur->mac_parameters.mac_default_lfn_key_index = index + 1;
-}
-
-static void ws_bootstrap_nw_frame_counter_set(struct net_if *cur, uint32_t counter, uint8_t slot)
-{
-    rcp_legacy_set_frame_counter(slot, counter);
-}
-
-static void ws_bootstrap_nw_frame_counter_read(struct net_if *cur, uint8_t slot)
-{
-    // Read frame counter
-    mac_helper_key_link_frame_counter_read(cur->id, slot);
-}
-
-static void ws_bootstrap_nw_info_updated(struct net_if *cur, uint16_t pan_id, uint16_t pan_version, uint16_t lfn_version)
-{
-    /* For border router, the PAE controller reads PAN ID, PAN version and network name from storage.
-     * If they are set, takes them into use here.
-     */
-    // Get network name
-    ws_gen_cfg_t gen_cfg;
-    if (ws_cfg_gen_get(&gen_cfg) < 0) {
-        return;
-    }
-
-    // If PAN ID has not been set, set it
-    if (cur->ws_info.network_pan_id == 0xffff) {
-        cur->ws_info.network_pan_id = pan_id;
-        // Sets PAN version
-        cur->ws_info.pan_information.pan_version = pan_version;
-        cur->ws_info.pan_information.pan_version_set = true;
-        cur->ws_info.pan_information.lfn_version = lfn_version;
-        cur->ws_info.pan_information.lfn_version_set = true;
-    }
-
-    // Stores the settings
-    ws_cfg_gen_set(cur, &gen_cfg, 0);
-}
-
-static bool ws_bootstrap_eapol_congestion_get(struct net_if *cur, uint16_t active_supp)
-{
-    if (cur == NULL || cur->random_early_detection == NULL || cur->llc_random_early_detection == NULL || cur->llc_eapol_random_early_detection == NULL) {
-        return false;
-    }
-
-    bool return_value = false;
-    static struct red_info *red_info = NULL;
-    uint16_t adaptation_average = 0;
-    uint16_t llc_average = 0;
-    uint16_t llc_eapol_average = 0;
-    uint16_t average_sum = 0;
-    uint8_t active_max = 0;
-    uint32_t heap_size = UINT32_MAX;
-
-    /*
-      * For different memory sizes the max simultaneous authentications will be
-      * 32k:    (32k / 50k) * 2 + 1 = 1
-      * 65k:    (65k / 50k) * 2 + 1 = 3
-      * 250k:   (250k / 50k) * 2 + 1 = 11
-      * 1000k:  (1000k / 50k) * 2 + 1 = 41
-      * 2000k:  (2000k / 50k) * 2 + 1 = 50 (upper limit)
-      */
-    active_max = (heap_size / 50000) * 2 + 1;
-    if (active_max > 50) {
-        active_max = 50;
-    }
-
-    // Read the values for adaptation and LLC queues
-    adaptation_average = random_early_detection_aq_read(cur->random_early_detection);
-    llc_average = random_early_detection_aq_read(cur->llc_random_early_detection);
-    llc_eapol_average  = random_early_detection_aq_read(cur->llc_eapol_random_early_detection);
-    // Calculate combined average
-    average_sum = adaptation_average + llc_average + llc_eapol_average;
-
-    // Maximum for active supplicants based on memory reached, fail
-    if (active_supp >= active_max) {
-        return_value = true;
-        goto congestion_get_end;
-    }
-
-    // Always allow at least five negotiations (if memory does not limit)
-    if (active_supp < 5) {
-        goto congestion_get_end;
-    }
-
-    if (red_info == NULL) {
-        red_info = random_early_detection_create(
-                       cur->ws_info.cfg->sec_prot.max_simult_sec_neg_tx_queue_min,
-                       cur->ws_info.cfg->sec_prot.max_simult_sec_neg_tx_queue_max,
-                       100, RED_AVERAGE_WEIGHT_DISABLED);
-    }
-    if (red_info == NULL) {
-        goto congestion_get_end;
-    }
-
-    // Check drop probability
-    average_sum = random_early_detection_aq_calc(red_info, average_sum);
-    return_value = random_early_detection_congestion_check(red_info);
-
-congestion_get_end:
-    tr_info("Active supplicant limit, active: %i max: %i summed averageQ: %i adapt averageQ: %i LLC averageQ: %i LLC EAPOL averageQ: %i drop: %s", active_supp, active_max, average_sum, adaptation_average, llc_average, llc_eapol_average, return_value ? "T" : "F");
-
-    return return_value;
-}
-
 /*
  * Event transitions
  *
@@ -1357,17 +1353,6 @@ static void ws_bootstrap_pan_config(struct net_if *cur)
 
     ws_stats_update(cur, STATS_WS_ASYNCH_TX_PC, 1);
     ws_llc_asynch_request(cur, &req);
-}
-
-static void ws_bootstrap_event_handler(struct event_payload *event)
-{
-    struct net_if *cur;
-    cur = protocol_stack_interface_info_get_by_bootstrap_id(event->receiver);
-    if (!cur) {
-        return;
-    }
-
-    ws_bootstrap_6lbr_event_handler(cur, event);
 }
 
 void ws_bootstrap_state_change(struct net_if *cur, icmp_state_e nwk_bootstrap_state)
