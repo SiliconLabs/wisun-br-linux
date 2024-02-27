84607e2b16620fe1de8f7de8e4e6a5594d9be8d5
Mathis Marion <mathis.marion@silabs.com>
2024-02-27 16:23:24 +0100

    rcp: drop unused fields
    Reviewed-by: Jérôme Pouiller <jerome.pouiller@silabs.com>
    Reviewed-by: Kylian Balan <kylian.balan@silabs.com>

--- 6lbr/app/rcp_api.h
+++ 6lbr/app/rcp_api.h
@@ -48,8 +48,6 @@ struct rcp {
     uint32_t version_api;
     uint32_t version_fw;
     const char *version_label;
-    uint32_t neighbors_table_size;
-    uint8_t lfn_limit;
     uint8_t  eui64[8];
     struct rcp_rail_config *rail_config_list;
 };
