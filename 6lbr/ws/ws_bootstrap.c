d9a01f76b956b5006501158ed692b656e62edc32
Kylian Balan <kylian.balan@silabs.com>
2024-03-07 15:02:46 +0100

    ws: drop useless ws_bootstrap_fhss_enable()
    WI_SUN-3147
    
    Reviewed-by: Mathis Marion <mathis.marion@silabs.com>

--- 6lbr/ws/ws_bootstrap.c
+++ 6lbr/ws/ws_bootstrap.c
@@ -137,11 +137,6 @@ void ws_bootstrap_fhss_configure_channel_masks(struct net_if *cur, fhss_ws_confi
                                 fhss_configuration->number_of_channels);
 }
 
-static int8_t ws_bootstrap_fhss_enable(struct net_if *cur)
-{
-    return 0;
-}
-
 bool ws_bootstrap_nd_ns_transmit(struct net_if *cur, ipv6_neighbour_t *entry,  bool unicast, uint8_t seq)
 {
     (void)cur;
@@ -454,7 +449,6 @@ int ws_bootstrap_set_domain_rf_config(struct net_if *cur)
 
 void ws_bootstrap_fhss_activate(struct net_if *cur)
 {
-    ws_bootstrap_fhss_enable(cur);
     rcp_set_filter_pan_id(cur->rcp, cur->ws_info.pan_information.pan_id);
     rcp_req_radio_enable(cur->rcp);
     return;
