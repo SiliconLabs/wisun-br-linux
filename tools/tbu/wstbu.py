54bb5c831a4da2c9fabfdc290f6d467d2c7dfb88
Mathis Marion <mathis.marion@silabs.com>
2024-03-20 18:43:06 +0100

    tbu: use multiprocessing module for frame subscription
    os.eventfd() requires Python 3.10 which has not enough availability in
    current Linux distributions. Python's multiprocessing module makes it
    much easier to terminate a task, even though it can be seen as a more
    dirty solution.

--- tools/tbu/wstbu.py
+++ tools/tbu/wstbu.py
@@ -12,15 +12,14 @@
 #
 import functools
 import ipaddress
+import multiprocessing
 import operator
 import os
-import select
 import shutil
 import socket
 import struct
 import sys
 import time
-import threading
 import typing
 
 import flask
@@ -671,45 +670,22 @@ def put_config_router():
     return error(400, WSTBU_ERR_NOT_ROUTER, 'unsupported endpoint')
 
 
-sub_eventfd = os.eventfd(0)
-sub_running = False
-sub_thread = None
-sub_fifo = None
+sub_process = None
 
 
 def subscription_frame_forward(family, sockaddr):
     ''' Read from the pcap capture FIFO and send into a UDP stream.'''
-    global sub_running
-    global sub_eventfd
-    global sub_fifo
 
-    sub_running = True
     sub_fifo = os.open(wsbrd.config['pcap_file'], os.O_RDONLY)
-    poll = select.poll()
-    poll.register(sub_fifo,    select.POLLIN)
-    poll.register(sub_eventfd, select.POLLIN)
     with socket.socket(family, socket.SOCK_DGRAM) as sck:
-        while sub_running:
-            for fd, revents in poll.poll():
-                if fd == sub_eventfd and revents & select.POLLIN:
-                    os.eventfd_read(sub_eventfd)
-                if fd == sub_fifo:
-                    if revents & select.POLLIN:
-                        data = os.read(sub_fifo, 2000)
-                        sck.sendto(data, sockaddr)
-                    elif revents & select.POLLHUP:
-                        utils.warn('subscription disconnected')
-                        sub_running = False
-    os.close(sub_fifo)
-    sub_fifo = None
+        while True:
+            data = os.read(sub_fifo, 2000)
+            sck.sendto(data, sockaddr)
 
 
 @json_errcheck('/subscription/frames')
 def put_subscription_frame():
-    global sub_running
-    global sub_eventfd
-    global sub_thread
-    global sub_fifo
+    global sub_process
 
     json = flask.request.get_json(force=True, silent=True)
     if json['subscriptionMode'] == 'Start':
@@ -720,32 +696,17 @@ def put_subscription_frame():
         except socket.gaierror as e:
             return error(500, WSTBU_ERR_UNKNOWN, f'getaddrinfo(address={addr}, port={port}): {e}')
         family, _, _, _, sockaddr = addrinfo[0]
-        if sub_fifo:
-            sub_running = False
-            os.eventfd_write(sub_eventfd, 1)
-            sub_thread.join()
-            sub_thread = None
-        elif sub_thread:
-            if sub_thread.is_alive():
-                sub_running = True
-            else:
-                sub_thread.join()
-                sub_thread = None
-        if not sub_thread:
-            sub_thread = threading.Thread(
-                target=subscription_frame_forward,
-                args=(family, sockaddr),
-                daemon=True
-            )
-            sub_thread.start()
+        if sub_process and sub_process.is_alive():
+            sub_process.terminate()
+        sub_process = multiprocessing.Process(
+            target=subscription_frame_forward,
+            args=(family, sockaddr),
+            daemon=True
+        )
+        sub_process.start()
     elif json['subscriptionMode'] == 'Stop':
-        if sub_thread:
-            sub_running = False
-            os.eventfd_write(sub_eventfd, 1)
-        # The thread may not be able to join now if it is blocking on FIFO open()
-        if sub_fifo:
-            sub_thread.join()
-            sub_thread = None
+        if sub_process and sub_process.is_alive():
+            sub_process.terminate()
     else:
         return error(400, WSTBU_ERR_UNKNOWN, 'invalid subscription mode')
     return success()
