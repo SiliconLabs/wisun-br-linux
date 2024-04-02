1e50ab7cec70e6697fbf53a529ed166b5ddcbba8
Mathis Marion <mathis.marion@silabs.com>
2024-04-02 09:59:09 +0200

    wsrd: add minimal main()
    Reviewed-by: Kylian Balan <kylian.balan@silabs.com>

---
+++ wsrd/app/wsrd.c
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2024 Silicon Laboratories Inc. (www.silabs.com)
+ *
+ * The licensor of this software is Silicon Laboratories Inc. Your use of this
+ * software is governed by the terms of the Silicon Labs Master Software License
+ * Agreement (MSLA) available at [1].  This software is distributed to you in
+ * Object Code format and/or Source Code format and is governed by the sections
+ * of the MSLA applicable to Object Code, Source Code and Modified Open Source
+ * Code. By using this software, you agree to the terms of the MSLA.
+ *
+ * [1]: https://www.silabs.com/about-us/legal/master-software-license-agreement
+ */
+#include <stdlib.h>
+
+#include "common/log.h"
+#include "common/version.h"
+
+int main(int argc, char *argv[])
+{
+    INFO("Silicon Labs Wi-SUN router %s", version_daemon_str);
+    return EXIT_SUCCESS;
+}
