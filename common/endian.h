0c93e345855fc068b16c3b24a601112ddfbf8f8b
Mathis Marion <mathis.marion@silabs.com>
2024-04-22 11:54:20 +0200

    utils: introduce typedefs for endianness
    Reviewed-by: Kylian Balan <kylian.balan@silabs.com>

--- common/endian.h
+++ common/endian.h
@@ -25,6 +25,15 @@
  * 802.15.4 protocols.
  */
 
+// Indicative types for C structs mapped to protocol packets. Use <endian.h>
+// or <arpa/inet.h>:              read              write
+typedef uint16_t be16_t; // be16toh()/ntohs() htobe16()/htons()
+typedef uint16_t le16_t; // le16toh()         htole16()
+typedef uint32_t be32_t; // be32toh()/ntohl() htobe32()/htonl()
+typedef uint32_t le32_t; // le32toh()         htole32()
+typedef uint64_t be64_t; // be64toh()         htobe64()
+typedef uint64_t le64_t; // le64toh()         htole64()
+
 uint16_t read_be16(const uint8_t ptr[2]);
 uint16_t read_le16(const uint8_t ptr[2]);
 uint24_t read_be24(const uint8_t ptr[3]);
