faacb7c2200532519487bd158d8cf83e62dd27d1
Kylian Balan <kylian.balan@silabs.com>
2024-05-21 12:10:38 +0200

    utils: introduce dhcp_opt_{push,fill}()
    Preparatory commit before dhcp client integration.
    
    WI_SUN-3278
    
    Reviewed-by: Mathis Marion <mathis.marion@silabs.com>

--- common/dhcp_common.c
+++ common/dhcp_common.c
@@ -16,6 +16,7 @@
 
 #include "common/specs/dhcpv6.h"
 #include "common/named_values.h"
+#include "common/endian.h"
 #include "common/iobuf.h"
 #include "common/log.h"
 
@@ -64,53 +65,75 @@ int dhcp_get_option(const uint8_t *data, size_t len, uint16_t option, struct iob
     return -ENOENT;
 }
 
+static int dhcp_opt_push(struct iobuf_write *buf, uint16_t opt)
+{
+    int offset;
+
+    iobuf_push_be16(buf, opt);
+    offset = buf->len;
+    // Length is filled by calling dhcp_opt_fill() with the returned offset.
+    iobuf_push_be16(buf, 0);
+    return offset;
+}
+
+static void dhcp_opt_fill(struct iobuf_write *buf, int offset)
+{
+    const size_t len = buf->len - offset - 2;
+
+    BUG_ON(buf->data[offset]);
+    BUG_ON(len > UINT16_MAX);
+    write_be16(buf->data + offset, len);
+}
+
 void dhcp_fill_client_id(struct iobuf_write *buf, uint16_t hwaddr_type, const uint8_t *hwaddr)
 {
+    int len_offset;
+
     BUG_ON(!hwaddr);
     BUG_ON(hwaddr_type != DHCPV6_DUID_HW_TYPE_EUI64 &&
            hwaddr_type != DHCPV6_DUID_HW_TYPE_IEEE802);
 
-    iobuf_push_be16(buf, DHCPV6_OPT_CLIENT_ID);
-    iobuf_push_be16(buf, 2 + 2 + 8);
+    len_offset = dhcp_opt_push(buf, DHCPV6_OPT_CLIENT_ID);
     iobuf_push_be16(buf, DHCPV6_DUID_TYPE_LINK_LAYER);
     iobuf_push_be16(buf, hwaddr_type);
     iobuf_push_data(buf, hwaddr, 8);
+    dhcp_opt_fill(buf, len_offset);
 }
 
 void dhcp_fill_rapid_commit(struct iobuf_write *buf)
 {
-    iobuf_push_be16(buf, DHCPV6_OPT_RAPID_COMMIT);
-    iobuf_push_be16(buf, 0);
+    int len_offset = dhcp_opt_push(buf, DHCPV6_OPT_RAPID_COMMIT);
+
+    dhcp_opt_fill(buf, len_offset);
 }
 
 void dhcp_fill_identity_association(struct iobuf_write *buf, uint32_t ia_id, const uint8_t ipv6[16],
                                     uint32_t preferred_lifetime, uint32_t valid_lifetime)
 {
-    uint16_t opt_len = 4 + 4 + 4;
+    int len_offset = dhcp_opt_push(buf, DHCPV6_OPT_IA_NA);
+    int addr_len_offset;
 
-    if (ipv6)
-        opt_len += 2 + 2 + 16 + 4 + 4;
-    iobuf_push_be16(buf, DHCPV6_OPT_IA_NA);
-    iobuf_push_be16(buf, opt_len);
     iobuf_push_be32(buf, ia_id);
     iobuf_push_be32(buf, 0); // T1
     iobuf_push_be32(buf, 0); // T2
     if (ipv6) {
-        iobuf_push_be16(buf, DHCPV6_OPT_IA_ADDRESS);
-        iobuf_push_be16(buf, 16 + 4 + 4);
+        addr_len_offset = dhcp_opt_push(buf, DHCPV6_OPT_IA_ADDRESS);
         iobuf_push_data(buf, ipv6, 16);
         iobuf_push_be32(buf, preferred_lifetime);
         iobuf_push_be32(buf, valid_lifetime);
+        dhcp_opt_fill(buf, addr_len_offset);
     }
+    dhcp_opt_fill(buf, len_offset);
 }
 
 void dhcp_fill_server_id(struct iobuf_write *buf, const uint8_t eui64[8])
 {
-    iobuf_push_be16(buf, DHCPV6_OPT_SERVER_ID);
-    iobuf_push_be16(buf, 2 + 2 + 8);
+    int len_offset = dhcp_opt_push(buf, DHCPV6_OPT_SERVER_ID);
+
     iobuf_push_be16(buf, DHCPV6_DUID_TYPE_LINK_LAYER);
     iobuf_push_be16(buf, DHCPV6_DUID_HW_TYPE_EUI64);
     iobuf_push_data(buf, eui64, 8);
+    dhcp_opt_fill(buf, len_offset);
 }
 
 int dhcp_check_status_code(const uint8_t *req, size_t req_len)
