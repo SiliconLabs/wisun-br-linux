039d838cc5028d10e5ebd3b15133795dd4a925e0
Kylian Balan <kylian.balan@silabs.com>
2024-05-21 12:10:25 +0200

    utils: use enums in common/specs/dhcpv6.h
    WI_SUN-3278
    
    Reviewed-by: Mathis Marion <mathis.marion@silabs.com>

--- common/specs/dhcpv6.h
+++ common/specs/dhcpv6.h
@@ -16,58 +16,68 @@
 
 // RFC3315 - Section 5.2
 // https://datatracker.ietf.org/doc/html/rfc3315#section-5.2
-#define DHCPV6_CLIENT_UDP_PORT 546
-#define DHCPV6_SERVER_UDP_PORT 547
+enum {
+    DHCPV6_CLIENT_UDP_PORT = 546,
+    DHCPV6_SERVER_UDP_PORT = 547,
+};
 
 // RFC3315 - Section 24.2
 // https://datatracker.ietf.org/doc/html/rfc3315#section-24.2
-#define DHCPV6_MSG_SOLICIT      1
-#define DHCPV6_MSG_ADVERT       2  /* Unused */
-#define DHCPV6_MSG_REQUEST      3  /* Unused */
-#define DHCPV6_MSG_CONFIRM      4  /* Unused */
-#define DHCPV6_MSG_RENEW        5  /* Unused */
-#define DHCPV6_MSG_REBIND       6  /* Unused */
-#define DHCPV6_MSG_REPLY        7
-#define DHCPV6_MSG_RELEASE      8  /* Unused */
-#define DHCPV6_MSG_DECLINE      9  /* Unused */
-#define DHCPV6_MSG_RECONFIGURE  10 /* Unused */
-#define DHCPV6_MSG_INFO_REQUEST 11 /* Unused */
-#define DHCPV6_MSG_RELAY_FWD    12
-#define DHCPV6_MSG_RELAY_REPLY  13
+enum {
+    DHCPV6_MSG_SOLICIT      = 1,
+    DHCPV6_MSG_ADVERT       = 2,  /* Unused */
+    DHCPV6_MSG_REQUEST      = 3,  /* Unused */
+    DHCPV6_MSG_CONFIRM      = 4,  /* Unused */
+    DHCPV6_MSG_RENEW        = 5,  /* Unused */
+    DHCPV6_MSG_REBIND       = 6,  /* Unused */
+    DHCPV6_MSG_REPLY        = 7,
+    DHCPV6_MSG_RELEASE      = 8,  /* Unused */
+    DHCPV6_MSG_DECLINE      = 9,  /* Unused */
+    DHCPV6_MSG_RECONFIGURE  = 10, /* Unused */
+    DHCPV6_MSG_INFO_REQUEST = 11, /* Unused */
+    DHCPV6_MSG_RELAY_FWD    = 12,
+    DHCPV6_MSG_RELAY_REPLY  = 13,
+};
 
 // RFC3315 - Section 24.3
 // https://datatracker.ietf.org/doc/html/rfc3315#section-24.3
-#define DHCPV6_OPT_CLIENT_ID                  1
-#define DHCPV6_OPT_SERVER_ID                  2
-#define DHCPV6_OPT_IA_NA                      3
-#define DHCPV6_OPT_IA_TA                      4  /* Unused */
-#define DHCPV6_OPT_IA_ADDRESS                 5
-#define DHCPV6_OPT_ORO                        6  /* Unused */
-#define DHCPV6_OPT_PREFERENCE                 7  /* Unused */
-#define DHCPV6_OPT_ELAPSED_TIME               8
-#define DHCPV6_OPT_RELAY                      9
-#define DHCPV6_OPT_RESERVED1                  10 /* Unused */
-#define DHCPV6_OPT_AUTH                       11 /* Unused */
-#define DHCPV6_OPT_UNICAST                    12 /* Unused */
-#define DHCPV6_OPT_STATUS_CODE                13
-#define DHCPV6_OPT_RAPID_COMMIT               14
-#define DHCPV6_OPT_USER_CLASS                 15 /* Unused */
-#define DHCPV6_OPT_VENDOR_CLASS               16 /* Unused */
-#define DHCPV6_OPT_VENDOR_SPECIFIC            17 /* Unused */
-#define DHCPV6_OPT_INTERFACE_ID               18
-#define DHCPV6_OPT_RECONF_MSG                 19 /* Unused */
-#define DHCPV6_OPT_RECONF_ACCEPT              20 /* Unused */
+enum {
+    DHCPV6_OPT_CLIENT_ID       = 1,
+    DHCPV6_OPT_SERVER_ID       = 2,
+    DHCPV6_OPT_IA_NA           = 3,
+    DHCPV6_OPT_IA_TA           = 4,  /* Unused */
+    DHCPV6_OPT_IA_ADDRESS      = 5,
+    DHCPV6_OPT_ORO             = 6,  /* Unused */
+    DHCPV6_OPT_PREFERENCE      = 7,  /* Unused */
+    DHCPV6_OPT_ELAPSED_TIME    = 8,
+    DHCPV6_OPT_RELAY           = 9,
+    DHCPV6_OPT_RESERVED1       = 10, /* Unused */
+    DHCPV6_OPT_AUTH            = 11, /* Unused */
+    DHCPV6_OPT_UNICAST         = 12, /* Unused */
+    DHCPV6_OPT_STATUS_CODE     = 13,
+    DHCPV6_OPT_RAPID_COMMIT    = 14,
+    DHCPV6_OPT_USER_CLASS      = 15, /* Unused */
+    DHCPV6_OPT_VENDOR_CLASS    = 16, /* Unused */
+    DHCPV6_OPT_VENDOR_SPECIFIC = 17, /* Unused */
+    DHCPV6_OPT_INTERFACE_ID    = 18,
+    DHCPV6_OPT_RECONF_MSG      = 19, /* Unused */
+    DHCPV6_OPT_RECONF_ACCEPT   = 20, /* Unused */
+};
 
 // RFC3315 - Section 24.5
 // https://datatracker.ietf.org/doc/html/rfc3315#section-24.5
-#define DHCPV6_DUID_TYPE_LINK_LAYER_PLUS_TIME 1 /* Unused */
-#define DHCPV6_DUID_TYPE_EN                   2 /* Unused */
-#define DHCPV6_DUID_TYPE_LINK_LAYER           3
-#define DHCPV6_DUID_TYPE_UUID                 4 /* Unused */
+enum {
+    DHCPV6_DUID_TYPE_LINK_LAYER_PLUS_TIME = 1, /* Unused */
+    DHCPV6_DUID_TYPE_EN                   = 2, /* Unused */
+    DHCPV6_DUID_TYPE_LINK_LAYER           = 3,
+    DHCPV6_DUID_TYPE_UUID                 = 4, /* Unused */
+};
 
 // Address Resolution Protocol (ARP) Parameters
 // https://www.iana.org/assignments/arp-parameters/arp-parameters.xhtml
-#define DHCPV6_DUID_HW_TYPE_IEEE802           6
-#define DHCPV6_DUID_HW_TYPE_EUI64             27
+enum {
+    DHCPV6_DUID_HW_TYPE_IEEE802 = 6,
+    DHCPV6_DUID_HW_TYPE_EUI64   = 27,
+};
 
 #endif
