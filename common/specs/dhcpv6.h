95d4a4514f6d5ebeb867e6e4a734222c84e15964
Kylian Balan <kylian.balan@silabs.com>
2024-05-21 12:10:24 +0200

    utils: convert all hexadecimal defines in common/specs/dhcpv6.h
    None of the referred specifications were using hexadecimal values for
    these defines.
    
    WI_SUN-3278
    
    Reviewed-by: Mathis Marion <mathis.marion@silabs.com>

--- common/specs/dhcpv6.h
+++ common/specs/dhcpv6.h
@@ -37,37 +37,37 @@
 
 // RFC3315 - Section 24.3
 // https://datatracker.ietf.org/doc/html/rfc3315#section-24.3
-#define DHCPV6_OPT_CLIENT_ID                  0x0001
-#define DHCPV6_OPT_SERVER_ID                  0x0002
-#define DHCPV6_OPT_IA_NA                      0x0003
-#define DHCPV6_OPT_IA_TA                      0x0004 /* Unused */
-#define DHCPV6_OPT_IA_ADDRESS                 0x0005
-#define DHCPV6_OPT_ORO                        0x0006 /* Unused */
-#define DHCPV6_OPT_PREFERENCE                 0x0007 /* Unused */
-#define DHCPV6_OPT_ELAPSED_TIME               0x0008
-#define DHCPV6_OPT_RELAY                      0x0009
-#define DHCPV6_OPT_RESERVED1                  0x000a /* Unused */
-#define DHCPV6_OPT_AUTH                       0x000b /* Unused */
-#define DHCPV6_OPT_UNICAST                    0x000c /* Unused */
-#define DHCPV6_OPT_STATUS_CODE                0x000d
-#define DHCPV6_OPT_RAPID_COMMIT               0x000e
-#define DHCPV6_OPT_USER_CLASS                 0x000f /* Unused */
-#define DHCPV6_OPT_VENDOR_CLASS               0x0010 /* Unused */
-#define DHCPV6_OPT_VENDOR_SPECIFIC            0x0011 /* Unused */
-#define DHCPV6_OPT_INTERFACE_ID               0x0012
-#define DHCPV6_OPT_RECONF_MSG                 0x0013 /* Unused */
-#define DHCPV6_OPT_RECONF_ACCEPT              0x0014 /* Unused */
+#define DHCPV6_OPT_CLIENT_ID                  1
+#define DHCPV6_OPT_SERVER_ID                  2
+#define DHCPV6_OPT_IA_NA                      3
+#define DHCPV6_OPT_IA_TA                      4  /* Unused */
+#define DHCPV6_OPT_IA_ADDRESS                 5
+#define DHCPV6_OPT_ORO                        6  /* Unused */
+#define DHCPV6_OPT_PREFERENCE                 7  /* Unused */
+#define DHCPV6_OPT_ELAPSED_TIME               8
+#define DHCPV6_OPT_RELAY                      9
+#define DHCPV6_OPT_RESERVED1                  10 /* Unused */
+#define DHCPV6_OPT_AUTH                       11 /* Unused */
+#define DHCPV6_OPT_UNICAST                    12 /* Unused */
+#define DHCPV6_OPT_STATUS_CODE                13
+#define DHCPV6_OPT_RAPID_COMMIT               14
+#define DHCPV6_OPT_USER_CLASS                 15 /* Unused */
+#define DHCPV6_OPT_VENDOR_CLASS               16 /* Unused */
+#define DHCPV6_OPT_VENDOR_SPECIFIC            17 /* Unused */
+#define DHCPV6_OPT_INTERFACE_ID               18
+#define DHCPV6_OPT_RECONF_MSG                 19 /* Unused */
+#define DHCPV6_OPT_RECONF_ACCEPT              20 /* Unused */
 
 // RFC3315 - Section 24.5
 // https://datatracker.ietf.org/doc/html/rfc3315#section-24.5
-#define DHCPV6_DUID_TYPE_LINK_LAYER_PLUS_TIME 0x0001 /* Unused */
-#define DHCPV6_DUID_TYPE_EN                   0x0002 /* Unused */
-#define DHCPV6_DUID_TYPE_LINK_LAYER           0x0003
-#define DHCPV6_DUID_TYPE_UUID                 0x0004 /* Unused */
+#define DHCPV6_DUID_TYPE_LINK_LAYER_PLUS_TIME 1 /* Unused */
+#define DHCPV6_DUID_TYPE_EN                   2 /* Unused */
+#define DHCPV6_DUID_TYPE_LINK_LAYER           3
+#define DHCPV6_DUID_TYPE_UUID                 4 /* Unused */
 
 // Address Resolution Protocol (ARP) Parameters
 // https://www.iana.org/assignments/arp-parameters/arp-parameters.xhtml
-#define DHCPV6_DUID_HW_TYPE_IEEE802           0x0006
-#define DHCPV6_DUID_HW_TYPE_EUI64             0x001b
+#define DHCPV6_DUID_HW_TYPE_IEEE802           6
+#define DHCPV6_DUID_HW_TYPE_EUI64             27
 
 #endif
