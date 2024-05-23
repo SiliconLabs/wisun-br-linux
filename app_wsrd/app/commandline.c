76ed7c7b36288bcb789ab8afb2775e830962998e
Kylian Balan <kylian.balan@silabs.com>
2024-05-23 16:22:15 +0200

    wsrd: add dhcp log option
    Preparatory commit before dhcp client integration.
    
    WI_SUN-3278

--- app_wsrd/app/commandline.c
+++ app_wsrd/app/commandline.c
@@ -31,6 +31,7 @@ const struct name_value valid_traces[] = {
     { "15.4-mngt",  TR_15_4_MNGT },
     { "ipv6",       TR_IPV6 },
     { "icmp",       TR_ICMP },
+    { "dhcp",       TR_DHCP },
     { "rpl",        TR_RPL },
     { "neigh-15.4", TR_NEIGH_15_4 },
     { "neigh-ipv6", TR_NEIGH_IPV6 },
@@ -54,7 +55,7 @@ void print_help(FILE *stream) {
     fprintf(stream, "Common options:\n");
     fprintf(stream, "  -u UART_DEVICE        Use UART bus\n");
     fprintf(stream, "  -T, --trace=TAG[,TAG] Enable traces marked with TAG. Valid tags: bus, cpc, hif, hif-extra,\n");
-    fprintf(stream, "                          15.4, 15.4-mngt, ipv6, rpl, neigh-15.4, neigh-ipv6, drop\n");
+    fprintf(stream, "                          15.4, 15.4-mngt, ipv6, dhcp, rpl, neigh-15.4, neigh-ipv6, drop\n");
     fprintf(stream, "  -F, --config=FILE     Read parameters from FILE. Command line options always have priority\n");
     fprintf(stream, "                          on config file\n");
     fprintf(stream, "  -o, --opt=PARM=VAL    Assign VAL to the parameter PARM. PARM can be any parameter accepted\n");
