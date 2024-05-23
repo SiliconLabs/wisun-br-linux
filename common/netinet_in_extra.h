f50bf4a196b928ae2ebc9f26451e37766d8f3d5c
Mathis Marion <mathis.marion@silabs.com>
2024-05-23 11:07:54 +0200

    utils: add parenthesis to IN6_IS_ADDR_UC_GLOBAL()

--- common/netinet_in_extra.h
+++ common/netinet_in_extra.h
@@ -21,10 +21,11 @@
  */
 
 // RFC 4291 - 2.4. Address Type Identification
-#define IN6_IS_ADDR_UC_GLOBAL(a)   \
+#define IN6_IS_ADDR_UC_GLOBAL(a) ( \
     !IN6_IS_ADDR_UNSPECIFIED(a) && \
     !IN6_IS_ADDR_LOOPBACK(a)    && \
     !IN6_IS_ADDR_MULTICAST(a)   && \
-    !IN6_IS_ADDR_LINKLOCAL(a)
+    !IN6_IS_ADDR_LINKLOCAL(a)      \
+)
 
 #endif
