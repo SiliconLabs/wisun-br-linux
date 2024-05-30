749d49f506a76563603d0a7019e3878a3a40c857
Kylian Balan <kylian.balan@silabs.com>
2024-05-30 17:43:54 +0200

    wsrd: introduce dbus_get_gaks()
    SADDLE-939
    WI_SUN-3384

--- app_wsrd/app/dbus.c
+++ app_wsrd/app/dbus.c
@@ -12,9 +12,26 @@
  */
 #include <systemd/sd-bus.h>
 
+#include "common/ws_keys.h"
 #include "app_wsrd/app/wsrd.h"
 
 #include "dbus.h"
+
+static int dbus_get_gaks(sd_bus *bus, const char *path, const char *interface,
+                         const char *property, sd_bus_message *reply,
+                         void *userdata, sd_bus_error *ret_error)
+{
+    struct wsrd *wsrd = userdata;
+    uint8_t gak[16];
+
+    // FIXME: get keys from supplicant
+    ws_generate_gak(wsrd->config.ws_netname, wsrd->config.ws_gtk, gak);
+
+    sd_bus_message_open_container(reply, 'a', "ay");
+    sd_bus_message_append_array(reply, 'y', gak, sizeof(gak));
+    sd_bus_message_close_container(reply);
+    return 0;
+}
 
 static int dbus_get_pan_id(sd_bus *bus, const char *path, const char *interface,
                            const char *property, sd_bus_message *reply,
@@ -38,5 +55,6 @@ const struct sd_bus_vtable wsrd_dbus_vtable[] = {
     SD_BUS_VTABLE_START(0),
     SD_BUS_PROPERTY("HwAddress",     "ay",  dbus_get_hw_address,     offsetof(struct wsrd, rcp.eui64),      0),
     SD_BUS_PROPERTY("PanId",         "q",   dbus_get_pan_id,         offsetof(struct wsrd, ws.pan_id),      0),
+    SD_BUS_PROPERTY("Gaks",          "aay", dbus_get_gaks,           0,                                     0),
     SD_BUS_VTABLE_END,
 };
