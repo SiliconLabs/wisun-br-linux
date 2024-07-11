97434b42bd5250aed36f8b37904f71d9d15dc569
Kylian Balan <kylian.balan@silabs.com>
2024-07-11 18:02:32 +0200

    utils: introduce kde_read_*()
    WI_SUN-3271

--- common/kde.c
+++ common/kde.c
@@ -10,11 +10,14 @@
  *
  * [1]: https://www.silabs.com/about-us/legal/master-software-license-agreement
  */
+#include <string.h>
+
 #include "common/specs/ieee80211.h"
 #include "common/specs/oui.h"
 #include "common/specs/ws.h"
 #include "common/endian.h"
 #include "common/pktbuf.h"
+#include "common/iobuf.h"
 
 #include "kde.h"
 
@@ -30,6 +33,98 @@ static void kde_write(struct pktbuf *buf, uint24_t oui, uint8_t data_type,
     write_be24(hdr.oui, oui);
     pktbuf_push_tail(buf, &hdr, sizeof(hdr));
     pktbuf_push_tail(buf, data, data_size);
+}
+
+static const uint8_t *kde_find(const uint8_t *data, int data_length, uint8_t type, uint24_t oui,
+                               uint8_t data_type, int size)
+{
+    struct iobuf_read input = {
+        .data = data,
+        .data_size = data_length,
+    };
+    const struct kde_hdr *hdr;
+
+    while (iobuf_remaining_size(&input)) {
+        hdr = (const struct kde_hdr *)iobuf_pop_data_ptr(&input, sizeof(struct kde_hdr));
+        if (!hdr)
+            return NULL;
+        if (hdr->type == type && read_be24(hdr->oui) == oui && iobuf_remaining_size(&input) >= size &&
+            hdr->data_type == data_type)
+            return iobuf_ptr(&input);
+        iobuf_pop_data(&input, NULL, hdr->length - 4); // length includes oui and data type
+    }
+    return NULL;
+}
+
+bool kde_read_lifetime(const uint8_t *data, int data_len, uint32_t *lifetime)
+{
+    const uint8_t *ptr = kde_find(data, data_len, IEEE80211_KDE_TYPE, OUI_IEEE80211,
+                                  IEEE80211_KDE_LIFETIME, sizeof(be32_t));
+
+    if (!ptr)
+        return false;
+
+    *lifetime = read_be32(ptr);
+    return true;
+}
+
+bool kde_read_lgtkl(const uint8_t *data, int data_len,  uint8_t *lgtkl)
+{
+    const uint8_t *ptr = kde_find(data, data_len, IEEE80211_KDE_TYPE, OUI_WISUN_ALLIANCE,
+                                  WS_KDE_LGTKL, sizeof(uint8_t));
+
+    if (!ptr)
+        return false;
+
+    *lgtkl = *ptr;
+    return true;
+}
+
+bool kde_read_lgtk(const uint8_t *data, int data_len, struct kde_gtk *lgtk_kde)
+{
+    const uint8_t *ptr = kde_find(data, data_len, IEEE80211_KDE_TYPE, OUI_WISUN_ALLIANCE,
+                                  WS_KDE_LGTK, sizeof(struct kde_gtk));
+
+    if (!ptr)
+        return false;
+
+    memcpy(lgtk_kde, ptr, sizeof(struct kde_gtk));
+    return true;
+}
+
+bool kde_read_gtkl(const uint8_t *data, int data_len, uint8_t *gtkl)
+{
+    const uint8_t *ptr = kde_find(data, data_len, IEEE80211_KDE_TYPE, OUI_WISUN_ALLIANCE,
+                                  WS_KDE_GTKL, sizeof(uint8_t));
+
+    if (!ptr)
+        return false;
+
+    *gtkl = *ptr;
+    return true;
+}
+
+bool kde_read_gtk(const uint8_t *data, int data_len, struct kde_gtk *gtk_kde)
+{
+    const uint8_t *ptr = kde_find(data, data_len, IEEE80211_KDE_TYPE, OUI_IEEE80211,
+                                  IEEE80211_KDE_GTK, sizeof(struct kde_gtk));
+
+    if (!ptr)
+        return false;
+
+    memcpy(gtk_kde, ptr, sizeof(struct kde_gtk));
+    return true;
+}
+
+bool kde_read_pmk_id(const uint8_t *data, int data_len, uint8_t pmkid[16])
+{
+    const uint8_t *ptr = kde_find(data, data_len, IEEE80211_KDE_TYPE, OUI_IEEE80211, IEEE80211_KDE_PMKID, 16);
+
+    if (!ptr)
+        return false;
+
+    memcpy(pmkid, ptr, 16);
+    return true;
 }
 
 void kde_write_pmkid(struct pktbuf *buf, const uint8_t pmkid[16])
--- common/kde.h
+++ common/kde.h
@@ -24,10 +24,26 @@ struct kde_hdr {
     uint8_t data_type;
 } __attribute__((packed));
 
+#define KDE_GTK_MASK_KEY_ID 0b00000011
+#define KDE_GTK_MASK_TX     0b00000100
+
+struct kde_gtk {
+    uint8_t flags;
+    uint8_t reserved;
+    uint8_t gtk[16];
+} __attribute__((packed));
+
 void kde_write_pmkid(struct pktbuf *buf, const uint8_t pmkid[16]);
 void kde_write_ptkid(struct pktbuf *buf, const uint8_t ptkid[16]);
 void kde_write_gtkl(struct pktbuf *buf, uint8_t gtkl);
 void kde_write_lgtkl(struct pktbuf *buf, uint8_t lgtkl);
 void kde_write_nr(struct pktbuf *buf, uint8_t node_role);
+
+bool kde_read_pmk_id(const uint8_t *data, int data_len, uint8_t pmkid[16]);
+bool kde_read_gtk(const uint8_t *data, int data_len, struct kde_gtk *gtk_kde);
+bool kde_read_gtkl(const uint8_t *data, int data_len, uint8_t *gtkl);
+bool kde_read_lgtk(const uint8_t *data, int data_len, struct kde_gtk *gtk_kde);
+bool kde_read_lgtkl(const uint8_t *data, int data_len,  uint8_t *lgtkl);
+bool kde_read_lifetime(const uint8_t *data, int data_len, uint32_t *lifetime);
 
 #endif
