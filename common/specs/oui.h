55bfbcc5811622ada4ad3bec3fbaf42fd1b09ec4
Kylian Balan <kylian.balan@silabs.com>
2024-07-22 14:20:50 +0200

    utils: introduce specs/oui.h
    WI_SUN-3272

---
+++ common/specs/oui.h
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2024 Silicon Laboratories Inc. (www.silabs.com)
+ *
+ * The licensor of this software is Silicon Laboratories Inc. Your use of this
+ * software is governed by the terms of the Silicon Labs Master Software License
+ * Agreement (MSLA) available at [1].  This software is distributed to you in
+ * Object Code format and/or Source Code format and is governed by the sections
+ * of the MSLA applicable to Object Code, Source Code and Modified Open Source
+ * Code. By using this software, you agree to the terms of the MSLA.
+ *
+ * [1]: https://www.silabs.com/about-us/legal/master-software-license-agreement
+ */
+#ifndef OUI_H
+#define OUI_H
+
+// See: https://standards-oui.ieee.org/
+enum {
+    OUI_IEEE80211      = 0x000fac,
+    OUI_WISUN_ALLIANCE = 0x0c5a9e,
+};
+
+#endif
