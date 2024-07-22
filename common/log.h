fb63720db1c663bfd6beffba8faf70b38fbfbc02
Kylian Balan <kylian.balan@silabs.com>
2024-07-22 14:20:59 +0200

    wsrd: introduce TR_SECURITY
    WI_SUN-3272

--- common/log.h
+++ common/log.h
@@ -92,6 +92,7 @@ enum {
     TR_IGNORE     = 0x00400000,
     TR_NEIGH_15_4 = 0x01000000,
     TR_NEIGH_IPV6 = 0x02000000,
+    TR_SECURITY   = 0x04000000,
 };
 #define TRACE(COND, ...)          __TRACE(COND, "" __VA_ARGS__)
 #define DEBUG(...)                __DEBUG("" __VA_ARGS__)
