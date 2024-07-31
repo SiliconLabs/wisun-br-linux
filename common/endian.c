12979955f86b56fca35b4b150365643c78ace55d
Mathis Marion <mathis.marion@silabs.com>
2024-07-31 16:26:49 +0200

    Revert "utils: leverage <endian.h> in common/endian.c"
    This reverts commit 0ac13e0a28507362770f9d4c4bac6b40e7e7bd4a.
    
    This has proven to generate SIGBUS on some platform (eg. 32-bit ARM on
    a Raspberry Pi). This is likely caused by dereferencing of an unaligned
    pointer. The previous implementation is not sensible to such issues.
    
    WI_SUN-3700

--- common/endian.c
+++ common/endian.c
@@ -10,103 +10,152 @@
  *
  * [1]: https://www.silabs.com/about-us/legal/master-software-license-agreement
  */
-#include <endian.h>
-#include <string.h>
-
 #include "endian.h"
 
 uint16_t read_be16(const uint8_t ptr[2])
 {
-    return be16toh(*(const be16_t *)ptr);
+    uint16_t val = ptr[0] << 8
+                 | ptr[1];
+    return val;
 }
 
 uint16_t read_le16(const uint8_t ptr[2])
 {
-    return le16toh(*(const le16_t *)ptr);
+    uint16_t val = ptr[1] << 8
+                 | ptr[0];
+    return val;
 }
 
 uint24_t read_be24(const uint8_t ptr[3])
 {
-    be32_t val = 0;
-
-    memcpy((uint8_t *)&val + 1, ptr, 3);
-    return be32toh(val);
+    uint24_t val = ptr[0] << 16
+                 | ptr[1] << 8
+                 | ptr[2];
+    return val;
 }
 
 uint24_t read_le24(const uint8_t ptr[3])
 {
-    le32_t val = 0;
-
-    memcpy(&val, ptr, 3);
-    return le32toh(val);
+    uint24_t val = ptr[2] << 16
+                 | ptr[1] << 8
+                 | ptr[0];
+    return val;
 }
 
 uint32_t read_be32(const uint8_t ptr[4])
 {
-    return be32toh(*(const be32_t *)ptr);
+    uint32_t val = ptr[0] << 24
+                 | ptr[1] << 16
+                 | ptr[2] << 8
+                 | ptr[3];
+    return val;
 }
 
 uint32_t read_le32(const uint8_t ptr[4])
 {
-    return le32toh(*(const le32_t *)ptr);
+    uint32_t val = ptr[3] << 24
+                 | ptr[2] << 16
+                 | ptr[1] << 8
+                 | ptr[0];
+    return val;
 }
 
 uint64_t read_be64(const uint8_t ptr[8])
 {
-    return be64toh(*(const be64_t *)ptr);
+    uint64_t val = (uint64_t)ptr[0] << 56
+                 | (uint64_t)ptr[1] << 48
+                 | (uint64_t)ptr[2] << 40
+                 | (uint64_t)ptr[3] << 32
+                 | (uint64_t)ptr[4] << 24
+                 | (uint64_t)ptr[5] << 16
+                 | (uint64_t)ptr[6] << 8
+                 | (uint64_t)ptr[7];
+    return val;
 }
 
 uint64_t read_le64(const uint8_t ptr[8])
 {
-    return le64toh(*(const le64_t *)ptr);
+    uint64_t val = (uint64_t)ptr[7] << 56
+                 | (uint64_t)ptr[6] << 48
+                 | (uint64_t)ptr[5] << 40
+                 | (uint64_t)ptr[4] << 32
+                 | (uint64_t)ptr[3] << 24
+                 | (uint64_t)ptr[2] << 16
+                 | (uint64_t)ptr[1] << 8
+                 | (uint64_t)ptr[0];
+    return val;
 }
 
 uint8_t *write_be16(uint8_t ptr[2], uint16_t val)
 {
-    *(be16_t *)ptr = htobe16(val);
-    return ptr + sizeof(val);
+    ptr[0] = val >> 8;
+    ptr[1] = val;
+    return ptr + 2;
 }
 
 uint8_t *write_le16(uint8_t ptr[2], uint16_t val)
 {
-    *(le16_t *)ptr = htole16(val);
-    return ptr + sizeof(val);
+    ptr[0] = val;
+    ptr[1] = val >> 8;
+    return ptr + 2;
 }
 
 uint8_t *write_be24(uint8_t ptr[3], uint24_t val)
 {
-    val = htobe32(val);
-    memcpy(ptr, (uint8_t *)&val + 1, 3);
+    ptr[0] = val >> 16;
+    ptr[1] = val >> 8;
+    ptr[2] = val;
     return ptr + 3;
 }
 
 uint8_t *write_le24(uint8_t ptr[3], uint24_t val)
 {
-    val = htole32(val);
-    memcpy(ptr, &val, 3);
+    ptr[0] = val;
+    ptr[1] = val >> 8;
+    ptr[2] = val >> 16;
     return ptr + 3;
 }
 
 uint8_t *write_be32(uint8_t ptr[4], uint32_t val)
 {
-    *(be32_t *)ptr = htobe32(val);
-    return ptr + sizeof(val);
+    ptr[0] = val >> 24;
+    ptr[1] = val >> 16;
+    ptr[2] = val >> 8;
+    ptr[3] = val;
+    return ptr + 4;
 }
 
 uint8_t *write_le32(uint8_t ptr[4], uint32_t val)
 {
-    *(le32_t *)ptr = htole32(val);
-    return ptr + sizeof(val);
+    ptr[0] = val;
+    ptr[1] = val >> 8;
+    ptr[2] = val >> 16;
+    ptr[3] = val >> 24;
+    return ptr + 4;
 }
 
 uint8_t *write_be64(uint8_t ptr[8], uint64_t val)
 {
-    *(be64_t *)ptr = htobe64(val);
-    return ptr + sizeof(val);
+    ptr[0] = val >> 56;
+    ptr[1] = val >> 48;
+    ptr[2] = val >> 40;
+    ptr[3] = val >> 32;
+    ptr[4] = val >> 24;
+    ptr[5] = val >> 16;
+    ptr[6] = val >> 8;
+    ptr[7] = val;
+    return ptr + 8;
 }
 
 uint8_t *write_le64(uint8_t ptr[8], uint64_t val)
 {
-    *(le64_t *)ptr = htole64(val);
-    return ptr + sizeof(val);
+    ptr[0] = val;
+    ptr[1] = val >> 8;
+    ptr[2] = val >> 16;
+    ptr[3] = val >> 24;
+    ptr[4] = val >> 32;
+    ptr[5] = val >> 40;
+    ptr[6] = val >> 48;
+    ptr[7] = val >> 56;
+    return ptr + 8;
 }
