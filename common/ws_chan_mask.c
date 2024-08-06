59b4555ae8beb6df5e5c728d9ecdf642de8fbcc9
Jérôme Pouiller <jerome.pouiller@silabs.com>
2024-08-06 11:21:37 +0200

    legal: add missing legal headers

--- common/ws_chan_mask.c
+++ common/ws_chan_mask.c
@@ -1,3 +1,16 @@
+/*
+ * SPDX-License-Identifier: LicenseRef-MSLA
+ * Copyright (c) 2024 Silicon Laboratories Inc. (www.silabs.com)
+ *
+ * The licensor of this software is Silicon Laboratories Inc. Your use of this
+ * software is governed by the terms of the Silicon Labs Master Software License
+ * Agreement (MSLA) available at [1].  This software is distributed to you in
+ * Object Code format and/or Source Code format and is governed by the sections
+ * of the MSLA applicable to Object Code, Source Code and Modified Open Source
+ * Code. By using this software, you agree to the terms of the MSLA.
+ *
+ * [1]: https://www.silabs.com/about-us/legal/master-software-license-agreement
+ */
 #include <errno.h>
 
 #include "common/bits.h"
