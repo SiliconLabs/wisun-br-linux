0a3b2a7c2f60d0c274d4664ceaff14b381f0ab92
Kylian Balan <kylian.balan@silabs.com>
2024-09-25 10:14:34 +0200

    conf: allow channel plan id 160 (CN)
    Fixes: 279ba86193 ("ws: update regulation with values from Wi-SUN TPS 2v01")

--- 6lbr/app/commandline.c
+++ 6lbr/app/commandline.c
@@ -146,6 +146,7 @@ static const int valid_ws_chan_plan_ids[] = {
     0x70, 0x71,                                           // WW
     0x80,                                                 // CN
     0x90,                                                 // CN
+    0xa0,                                                 // CN
     INT_MIN
 };
 
