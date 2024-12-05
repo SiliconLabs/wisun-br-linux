c39597aaae188ae9002b756148237eb8502fd25a
Mathis Marion <mathis.marion@silabs.com>
2024-12-05 15:44:10 +0100

    utils: RADIUS demo stub MAC with sockets
    The previous solution created some unexpected execution paths, and
    potentially big call stacks because execution could go back and forth
    between the supp and the auth without a function return.
    
    All packets are prefixed with a header composed of the supplicant EUI-64
    and the KMP ID.

--- tools/demo/eapol.c
+++ tools/demo/eapol.c
@@ -26,15 +26,30 @@
 
 struct ctx {
     struct supplicant_ctx supp;
+    struct sockaddr_storage supp_addr;
+    int supp_fd;
+
     struct auth_ctx auth;
+    int auth_fd;
 };
 
 static void supp_sendto_mac(struct supplicant_ctx *supp, uint8_t kmp_id,
                             const void *buf, size_t buf_len, const uint8_t dst[8])
 {
     struct ctx *ctx = container_of(supp, struct ctx, supp);
+    struct iovec iov[] = {
+        { .iov_base = supp->eui64, .iov_len = 8 },
+        { .iov_base = &kmp_id,     .iov_len = 1 },
+        { .iov_base = (void *)buf, .iov_len = buf_len },
+    };
+    struct msghdr msg = {
+        .msg_iov    = iov,
+        .msg_iovlen = 3,
+    };
+    ssize_t ret;
 
-    auth_recv_eapol(&ctx->auth, kmp_id, (struct eui64 *)supp->eui64, buf, buf_len);
+    ret = sendmsg(ctx->supp_fd, &msg, 0);
+    FATAL_ON(ret < 8 + 1 + buf_len, 2, "sendmsg: %m");
 }
 
 static uint8_t *supp_get_target(struct supplicant_ctx *supp)
@@ -62,8 +77,21 @@ static void auth_sendto_mac(struct auth_ctx *auth, uint8_t kmp_id,
                             const void *buf, size_t buf_len, const struct eui64 *dst)
 {
     struct ctx *ctx = container_of(auth, struct ctx, auth);
+    struct iovec iov[] = {
+        { .iov_base = (void *)dst, .iov_len = 8 },
+        { .iov_base = &kmp_id,     .iov_len = 1 },
+        { .iov_base = (void *)buf, .iov_len = buf_len },
+    };
+    struct msghdr msg = {
+        .msg_name    = &ctx->supp_addr,
+        .msg_namelen = sizeof(ctx->supp_addr),
+        .msg_iov     = iov,
+        .msg_iovlen  = 3,
+    };
+    ssize_t ret;
 
-    supp_recv_eapol(&ctx->supp, kmp_id, buf, buf_len, auth->eui64.u8);
+    ret = sendmsg(ctx->auth_fd, &msg, 0);
+    FATAL_ON(ret < 8 + 1 + buf_len, 2, "sendmsg: %m");
 }
 
 static void auth_on_supp_gtk_installed(struct auth_ctx *auth, const struct eui64 *eui64, uint8_t index)
@@ -76,13 +104,15 @@ int main()
 {
     const struct eui64 auth_eui64 = { .u8 = { [7] = 1 } };
     const struct eui64 supp_eui64 = { .u8 = { [7] = 2 } };
+    struct sockaddr_in6 auth_addr = { };
     struct in6_addr radius_addr = { };
     struct storage_parse_info info;
     struct iovec supp_cert = { };
     struct iovec supp_key = { };
     struct iovec ca_cert = { };
-    struct pollfd pfd[2] = { };
-    int ret;
+    struct pollfd pfd[4] = { };
+    uint8_t buf[2048];
+    ssize_t ret;
     struct ctx ctx = {
         .supp.key_request_txalg.rand_min = -0.1,
         .supp.key_request_txalg.irt_s    = 1,
@@ -122,16 +152,49 @@ int main()
 
     supp_start_key_request(&ctx.supp);
 
+    ctx.auth_fd = socket(AF_INET6, SOCK_DGRAM, IPPROTO_UDP);
+    FATAL_ON(ctx.supp_fd < 0, 2, "socket: %m");
+    auth_addr.sin6_family = AF_INET6;
+    auth_addr.sin6_addr = in6addr_loopback;
+    auth_addr.sin6_port = htons(10000);
+    ret = bind(ctx.auth_fd, (struct sockaddr *)&auth_addr, sizeof(auth_addr));
+    FATAL_ON(ret < 0, 2, "bind: %m");
+
+    ctx.supp_fd = socket(AF_INET6, SOCK_DGRAM, IPPROTO_UDP);
+    FATAL_ON(ctx.supp_fd < 0, 2, "socket: %m");
+    ret = connect(ctx.supp_fd, (struct sockaddr *)&auth_addr, sizeof(auth_addr));
+    FATAL_ON(ret < 0, 2, "connect: %m");
+    ret = getsockname(ctx.supp_fd, (struct sockaddr *)&ctx.supp_addr,
+                      (socklen_t[1]){ sizeof(ctx.supp_addr) });
+    FATAL_ON(ret < 0, 2, "getsockname: %m");
+
     pfd[0].fd = timer_fd();
     pfd[0].events = POLLIN;
     pfd[1].fd = ctx.auth.radius_fd;
     pfd[1].events = POLLIN;
+    pfd[2].fd = ctx.auth_fd;
+    pfd[2].events = POLLIN;
+    pfd[3].fd = ctx.supp_fd;
+    pfd[3].events = POLLIN;
     while (1) {
-        ret = poll(pfd, 2, -1);
+        ret = poll(pfd, 4, -1);
         FATAL_ON(ret < 0, 2, "poll: %m");
         if (pfd[0].revents & POLLIN)
             timer_process();
         if (pfd[1].revents & POLLIN)
             radius_recv(&ctx.auth);
+        if (pfd[2].revents & POLLIN) {
+            ret = recv(ctx.auth_fd, buf, sizeof(buf), 0);
+            FATAL_ON(ret < 8 + 1, 2, "recv: %m");
+            auth_recv_eapol(&ctx.auth, buf[8], (struct eui64 *)buf,
+                            buf + 8 + 1, ret - 8 - 1);
+        }
+        if (pfd[3].revents & POLLIN) {
+            ret = recv(ctx.supp_fd, buf, sizeof(buf), 0);
+            FATAL_ON(ret < 8 + 1, 2, "recv: %m");
+            supp_recv_eapol(&ctx.supp, buf[8],
+                            buf + 8 + 1, ret - 8 - 1,
+                            ctx.auth.eui64.u8);
+        }
     }
 }
