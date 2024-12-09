691816762a0115511ce9338a728680ceea624b9b
Mathis Marion <mathis.marion@silabs.com>
2024-12-09 15:17:03 +0100

    utils: add CLI to RADIUS demo

--- tools/demo/eapol.c
+++ tools/demo/eapol.c
@@ -11,8 +11,10 @@
  *
  * [1]: https://www.silabs.com/about-us/legal/master-software-license-agreement
  */
+#define _DEFAULT_SOURCE
 #include <arpa/inet.h>
 #include <sys/random.h>
+#include <getopt.h>
 #include <poll.h>
 #include <string.h>
 
@@ -130,36 +132,101 @@ ssize_t __wrap_send(int fd, const void *buf, size_t buf_len, int flags)
     return __real_send(fd, buf, buf_len, flags);
 }
 
-static void init(struct ctx *ctx)
+static void help(void)
+{
+    INFO("Wi-SUN authenticator and suplicant demo involving EAPoL, EAP-TLS,");
+    INFO("IEEE 802.11 4WH/GKH, and RADIUS.");
+    INFO("Usage:");
+    INFO("    demo-eapol-radius [opts]");
+    INFO("Available options:");
+    INFO("    -h --help");
+    INFO("    -r --radius-server=ADDRESS");
+    INFO("    -s --radius-secret=STRING");
+    INFO("    -A --supp-ca=FILE   Certificate authority used to verify the TLS server certificate");
+    INFO("                        default=/usr/local/share/doc/wsbrd/examples/ca_cert.pem");
+    INFO("    -C --supp-cert=FILE Supplicant certificate validated by the TLS server");
+    INFO("                        default=/usr/local/share/doc/wsbrd/examples/node_cert.pem");
+    INFO("    -K --supp-key=FILE  Supplicant private key");
+    INFO("                        default=/usr/local/share/doc/wsbrd/examples/node_key.pem");
+}
+
+static void init(struct ctx *ctx, int argc, char *argv[])
 {
     const struct eui64 auth_eui64 = { .u8 = { [7] = 1 } };
     const struct eui64 supp_eui64 = { .u8 = { [7] = 2 } };
-    struct sockaddr_in6 radius_addr = { };
+    struct sockaddr_storage radius_addr = { };
     struct sockaddr_in6 auth_addr = { };
     struct storage_parse_info info;
     struct iovec supp_cert = { };
     struct iovec supp_key = { };
     struct iovec ca_cert = { };
-    int ret;
+    int ret, opt;
+    const char *opts_short = "hr:s:A:C:K:";
+    const struct option opts_long[] = {
+        { "help",          no_argument,       0, 'h' },
+        { "radius-server", required_argument, 0, 'r' },
+        { "radius-secret", required_argument, 0, 's' },
+        { "supp-ca",       required_argument, 0, 'A' },
+        { "supp-cert",     required_argument, 0, 'C' },
+        { "supp-key",      required_argument, 0, 'K' },
+        { }
+    };
 
     g_enabled_traces |= TR_DROP;
     g_enabled_traces |= TR_SECURITY;
 
-    srand(0); // Fixed seed
-
+    strcpy(info.filename, "commandline");
     strcpy(info.value, "/usr/local/share/doc/wsbrd/examples/ca_cert.pem");
     conf_set_pem(&info, &ca_cert, NULL);
     strcpy(info.value, "/usr/local/share/doc/wsbrd/examples/node_cert.pem");
     conf_set_pem(&info, &supp_cert, NULL);
     strcpy(info.value, "/usr/local/share/doc/wsbrd/examples/node_key.pem");
     conf_set_pem(&info, &supp_key, NULL);
+
+    while ((opt = getopt_long(argc, argv, opts_short, opts_long, NULL)) != -1) {
+        for (const struct option *opt_long = opts_long; opt_long->name; opt_long++)
+            if (opt == opt_long->val)
+                strcpy(info.key, opt_long->name);
+        if (optarg)
+            strncpy(info.value, optarg, sizeof(info.value) - 1);
+        switch (opt) {
+        case 'r':
+            conf_set_netaddr(&info, &radius_addr, NULL);
+            break;
+        case 's':
+            strncpy(ctx->auth.radius_secret, optarg, sizeof(ctx->auth.radius_secret) - 1);
+            break;
+        case 'A':
+            conf_set_pem(&info, &ca_cert, NULL);
+            break;
+        case 'C':
+            conf_set_pem(&info, &supp_cert, NULL);
+            break;
+        case 'K':
+            conf_set_pem(&info, &supp_key, NULL);
+            break;
+        case 'h':
+            help();
+            exit(EXIT_SUCCESS);
+        default:
+            help();
+            exit(EXIT_FAILURE);
+        }
+    }
+    if (radius_addr.ss_family != AF_UNSPEC && !ctx->auth.radius_secret[0])
+        FATAL(1, "missing --radius-secret");
+    if (radius_addr.ss_family == AF_UNSPEC && ctx->auth.radius_secret[0])
+        FATAL(1, "missing --radius-server");
+
+    srand(0); // Fixed seed
+
     supp_init(&ctx->supp, &ca_cert, &supp_cert, &supp_key, supp_eui64.u8);
     supp_reset(&ctx->supp);
 
-    strcpy(ctx->auth.radius_secret, "SHARED_SECRET");
-    radius_addr.sin6_family = AF_INET6;
-    inet_pton(AF_INET6, "::1", &radius_addr.sin6_addr);
-    radius_init(&ctx->auth, (struct sockaddr *)&radius_addr);
+    if (radius_addr.ss_family != AF_UNSPEC)
+        radius_init(&ctx->auth, (struct sockaddr *)&radius_addr);
+    else
+        WARN("no RADIUS server configured");
     auth_start(&ctx->auth, &auth_eui64);
 
     supp_start_key_request(&ctx->supp);
@@ -181,7 +248,7 @@ static void init(struct ctx *ctx)
     FATAL_ON(ret < 0, 2, "getsockname: %m");
 }
 
-int main()
+int main(int argc, char *argv[])
 {
     struct pollfd pfd[4] = { };
     uint8_t buf[2048];
@@ -209,7 +276,7 @@ int main()
         .auth.timeout_ms = 500,
     };
 
-    init(&ctx);
+    init(&ctx, argc, argv);
 
     pfd[0].fd = timer_fd();
     pfd[0].events = POLLIN;
