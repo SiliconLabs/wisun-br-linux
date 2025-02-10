44dc6f3dd41ead554a350c15daefdbf82b8fabf4
Kylian Balan <kylian.balan@silabs.com>
2025-02-10 12:52:48 +0100

    tls: introduce tls_load_pem()
    WISeKey intermediate CAs use an X509v3 Certificate Policy extension with
    a Wi-SUN Alliance OID. This extension is marked as "critical" and
    verification cannot be done by Mbed TLS, it needs a callback for that.
    mbedtls_x509_crt_parse() does not offers this capability, only
    mbedtls_x509_crt_parse_der_with_ext_cb() does.
    
    WI_SUN-2275

--- common/crypto/tls.h
+++ common/crypto/tls.h
@@ -80,6 +80,7 @@ int tls_send(void *ctx, const unsigned char *buf, size_t len);
 int tls_recv(void *ctx, unsigned char *buf, size_t len);
 void tls_install_pmk(struct tls_client_ctx *tls_client, const uint8_t key[32]);
 void tls_init_client(struct tls_ctx *tls, struct tls_client_ctx *tls_client);
+int tls_load_pem(struct mbedtls_x509_crt *cert, const uint8_t *buf, size_t buf_len);
 void tls_init(struct tls_ctx *tls, int endpoint, const struct iovec *ca_cert, const struct iovec *cert,
               const struct iovec *key);
 
