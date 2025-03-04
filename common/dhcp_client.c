e06ee133fd16513b42c06f4c55fefa0c755a347d
Mathis Marion <mathis.marion@silabs.com>
2025-03-04 16:15:38 +0100

    dhcp: fix missing recvfrom() errcheck
    Also remove useless condition on address type (socket is opened with
    AF_INET6).
    
    Coverity reported:
    
        The negative value may be unexpected by later operations, causing incorrect computations.
        In dhcp_recv: Negative value can be returned from function is not being checked before being used improperly (CWE-394)
        In dhcp_client_recv: Negative value can be returned from function is not being checked before being used improperly (CWE-394)

--- common/dhcp_client.c
+++ common/dhcp_client.c
@@ -239,8 +239,8 @@ void dhcp_client_recv(struct dhcp_client *client)
 
     req.data = buf;
     req.data_size = xrecvfrom(client->fd, buf, sizeof(buf), 0, (struct sockaddr *)&src_addr, &src_addr_len);
-    if (src_addr.sin6_family != AF_INET6) {
-        TRACE(TR_DROP, "drop %-9s: not IPv6", "dhcp");
+    if (req.data_size < 0) {
+        WARN("%s: recvfrom: %m", __func__);
         return;
     }
 
