6818f379c9a690df4133ce30c0465660956c25b8
Kylian Balan <kylian.balan@silabs.com>
2025-03-26 15:26:05 +0100

    fixup! wsrd: prevent reception of DIOs in join state < 4

--- app_wsrd/ipv6/rpl.c
+++ app_wsrd/ipv6/rpl.c
@@ -12,6 +12,7 @@
  * [1]: https://www.silabs.com/about-us/legal/master-software-license-agreement
  */
 #define _GNU_SOURCE
+#include <unistd.h>
 #include <sys/socket.h>
 #include <netinet/icmp6.h>
 #include <netinet/in.h>
