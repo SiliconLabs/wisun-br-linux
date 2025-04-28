4d8c49deb3781946c1df55ec449fb4ec1efb865a
Mathis Marion <mathis.marion@silabs.com>
2025-04-28 12:19:11 +0200

    simu: drop wsrd_ns3_main()
    WI_SUN-4195

--- tools/simulation/wsrd_ns3.cpp
+++ tools/simulation/wsrd_ns3.cpp
@@ -31,53 +31,11 @@ extern "C" {
 #include "common/version.h"
 }
 
-#if SL_NS3_WISUN_LINUX_VERSION < VERSION(2, 2, 0) || SL_NS3_WISUN_LINUX_VERSION >= VERSION(3, 0, 0)
+#if SL_NS3_WISUN_LINUX_VERSION < VERSION(2, 3, 0) || SL_NS3_WISUN_LINUX_VERSION >= VERSION(3, 0, 0)
 #error "Incompatible ns-3 version"
 #endif
 
 int g_simulation_id;
-std::string g_capture_filename;
-std::string g_capture_init_filename; // Unused
-
-static void wsrd_ns3_cleanup(void *arg)
-{
-    char *config_filename = (char *)arg;
-    int ret;
-
-    ret = unlink(config_filename);
-    WARN_ON(ret, "unlink %s: %m", config_filename);
-}
-
-void wsrd_ns3_main(const char *config)
-{
-    char config_filename[] = "/tmp/wsbrd-ns3-XXXXXX.conf";
-    std::vector<char *> args;
-    int config_fd;
-    ssize_t size;
-
-    BUG_ON(g_uart_cb.IsNull());
-    BUG_ON(g_uart_fd < 0);
-
-    config_fd = mkstemps(config_filename, strlen(".conf"));
-    FATAL_ON(config_fd < 0, 1, "mkstemps: %m");
-    size = write(config_fd, config, strlen(config));
-    FATAL_ON(size < 0, 1, "write %s: %m", config_filename);
-    if ((size_t)size < strlen(config))
-        FATAL(1, "write %s: Short write", config_filename);
-    close(config_fd);
-
-    // Cast to non-const, wsbrd is trusted to not modify its arguments
-    args.push_back((char *)"wsrd");
-    args.push_back((char *)"-F");
-    args.push_back(config_filename);
-    args.push_back((char *)"-u/dev/null"); // Provide a UART devive so parse_commandline succeeds
-    args.push_back((char *)"-D");
-    args.push_back(NULL);
-
-    pthread_cleanup_push(wsrd_ns3_cleanup, config_filename);
-    wsrd_main(args.size() - 1, (char **)args.data()); // Does not return
-    pthread_cleanup_pop(true);
-}
 
 static char last_error[256];
 extern "C" void __wrap___tr_printf(const char *color, const char *fmt, ...)
