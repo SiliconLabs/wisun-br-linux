02773cdb5fbfee5f42ab1fec512cf783d71e994e
Mathis Marion <mathis.marion@silabs.com>
2025-09-10 16:12:33 +0200

    simu: set error_code field of socket cnf messages
    WI_SUN-4682
    Co-authored-by: Mathis Marion <mathi.marion@silabs.com>

--- tools/simulation/ncp_socket.c
+++ tools/simulation/ncp_socket.c
@@ -22,6 +22,7 @@
 
 #include "tools/simulation/ncp_ind.h"
 #include "tools/simulation/ncp_values.h"
+#include "common/endian.h"
 #include "common/log.h"
 
 #include "ncp_socket.h"
@@ -69,6 +70,13 @@ static void ncp_sk_exit(void)
     pthread_join(g_sk_thread, NULL);
 }
 
+// NOTE: using void * prevents -Waddress-of-packed-member
+static inline void ncp_sk_set_result(int err, void *status, void *error_code)
+{
+    write_le32(status, ncp_status(err));
+    write_le32(error_code, ncp_errno(err));
+}
+
 void ncp_sk_open(const void *_req, const void *req_data, void *_cnf, void *cnf_data)
 {
     static const struct ncp_val domains[] = {
@@ -97,7 +105,8 @@ void ncp_sk_open(const void *_req, const void *req_data, void *_cnf, void *cnf_d
         type |= SOCK_NONBLOCK;
 
     cnf->body.socket_id = socket(domain, type, proto);
-    cnf->body.status = htole32(cnf->body.socket_id < 0 ? ncp_status(errno) : SL_STATUS_OK);
+    ncp_sk_set_result(cnf->body.socket_id < 0 ? errno : 0,
+                      &cnf->body.status, &cnf->body.error_code);
 }
 
 void ncp_sk_close(const void *_req, const void *req_data, void *_cnf, void *cnf_data)
@@ -107,7 +116,8 @@ void ncp_sk_close(const void *_req, const void *req_data, void *_cnf, void *cnf_
     int ret;
 
     ret = close(req->body.socket_id);
-    cnf->body.status = htole32(ret < 0 ? ncp_status(errno) : SL_STATUS_OK);
+    ncp_sk_set_result(ret < 0 ? errno : 0,
+                      &cnf->body.status, &cnf->body.error_code);
 }
 
 void ncp_sk_bind(const void *_req, const void *req_data, void *_cnf, void *cnf_data)
@@ -122,7 +132,8 @@ void ncp_sk_bind(const void *_req, const void *req_data, void *_cnf, void *cnf_d
     int ret;
 
     ret = bind(req->body.socket_id, (struct sockaddr *)&sin6, sizeof(sin6));
-    cnf->body.status = htole32(ret < 0 ? ncp_status(errno) : SL_STATUS_OK);
+    ncp_sk_set_result(ret < 0 ? errno : 0,
+                      &cnf->body.status, &cnf->body.error_code);
 }
 
 void ncp_sk_send(const void *_req, const void *req_data, void *_cnf, void *cnf_data)
@@ -132,7 +143,8 @@ void ncp_sk_send(const void *_req, const void *req_data, void *_cnf, void *cnf_d
     ssize_t ret;
 
     ret = send(req->body.socket_id, req_data, req->body.data_length, 0);
-    cnf->body.status = htole32(ret < 0 ? ncp_status(errno) : SL_STATUS_OK);
+    ncp_sk_set_result(ret < 0 ? errno : 0,
+                      &cnf->body.status, &cnf->body.error_code);
 }
 
 static void ncp_sk_setopt_evtmode(const sl_wisun_msg_set_socket_option_req_t *req,
@@ -143,7 +155,7 @@ static void ncp_sk_setopt_evtmode(const sl_wisun_msg_set_socket_option_req_t *re
     int ret;
 
     if (req->body.option_length != sizeof(*mode)) {
-        cnf->body.status = SL_STATUS_INVALID_PARAMETER;
+        ncp_sk_set_result(EINVAL, &cnf->body.status, &cnf->body.error_code);
         return;
     }
 
@@ -157,10 +169,11 @@ static void ncp_sk_setopt_evtmode(const sl_wisun_msg_set_socket_option_req_t *re
         event.data.fd = req->body.socket_id;
         ret = epoll_ctl(g_sk_epfd, EPOLL_CTL_ADD, req->body.socket_id, &event);
         FATAL_ON(ret < 0, 2, "epoll_ctl ADD %i: %m", req->body.socket_id);
+        ncp_sk_set_result(0, &cnf->body.status, &cnf->body.error_code);
         break;
     case 1: // SL_WISUN_SOCKET_EVENT_MODE_POLLING
     default:
-        cnf->body.status = SL_STATUS_NOT_SUPPORTED;
+        ncp_sk_set_result(ENOTSUP, &cnf->body.status, &cnf->body.error_code);
         break;
     }
 }
@@ -183,9 +196,11 @@ void ncp_sk_setopt(const void *_req, const void *req_data, void *_cnf, void *cnf
     }
 
     if (level < 0 || optname < 0) {
-        cnf->body.status = SL_STATUS_NOT_SUPPORTED;
+        ncp_sk_set_result(ENOTSUP, &cnf->body.status, &cnf->body.error_code);
     } else {
-        ret = setsockopt(req->body.socket_id, level, optname, req_data, req->body.option_length);
-        cnf->body.status = ret < 0 ? ncp_status(errno) : SL_STATUS_OK;
+        ret = setsockopt(req->body.socket_id, level, optname,
+                         req_data, req->body.option_length);
+        ncp_sk_set_result(ret < 0 ? errno : 0,
+                          &cnf->body.status, &cnf->body.error_code);
     }
 }
