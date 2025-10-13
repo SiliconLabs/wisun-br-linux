40598719ebd0375177f7eb630f56f81553568b95
Mathis Marion <mathis.marion@silabs.com>
2025-10-13 16:21:44 +0200

    simu: add sl_wisun_msg_api.h stub header
    This header used to be in the src/ directory which made it easier to
    override included headers. Now it will be put into inc/ so the
    preprocessor will prefer picking headers in the same directory, thus
    we need a stub header.

---
+++ tools/simulation/include/sl_wisun_msg_api.h
@@ -0,0 +1,12 @@
+#ifndef NS3_SL_WISUN_SOCKET_API_H
+#define NS3_SL_WISUN_SOCKET_API_H
+
+/*
+ * Include first to select stub header. Otherwise the C preprocessor will pick
+ * the real header due to prefering relative paths.
+ */
+#include <sl_wisun_types.h>
+
+#include_next <sl_wisun_msg_api.h>
+
+#endif
