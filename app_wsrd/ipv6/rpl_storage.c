99cc31ea2043f3000dff9add1753eae0aaab36ac
Kylian Balan <kylian.balan@silabs.com>
2025-11-19 14:19:20 +0100

    wsrd/rpl: introduce rpl_storage.{c,h}
    WI_SUN-4898

---
+++ app_wsrd/ipv6/rpl_storage.c
@@ -0,0 +1,60 @@
+/*
+ * SPDX-License-Identifier: LicenseRef-MSLA
+ * Copyright (c) 2024 Silicon Laboratories Inc. (www.silabs.com)
+ *
+ * The licensor of this software is Silicon Laboratories Inc. Your use of this
+ * software is governed by the terms of the Silicon Labs Master Software License
+ * Agreement (MSLA) available at [1].  This software is distributed to you in
+ * Object Code format and/or Source Code format and is governed by the sections
+ * of the MSLA applicable to Object Code, Source Code and Modified Open Source
+ * Code. By using this software, you agree to the terms of the MSLA.
+ *
+ * [1]: https://www.silabs.com/about-us/legal/master-software-license-agreement
+ */
+#include <fnmatch.h>
+#include <unistd.h>
+
+#include "common/key_value_storage.h"
+#include "common/log.h"
+
+#include "rpl.h"
+#include "rpl_storage.h"
+
+bool rpl_storage_load(struct rpl_ctx *rpl)
+{
+    struct storage_parse_info *info;
+    int ret;
+
+    info = storage_open_prefix("rpl", "r");
+    if (!info)
+        return false;
+
+    while (true) {
+        ret = storage_parse_line(info);
+        if (ret == EOF)
+            break;
+        if (ret) {
+            WARN("%s:%d: invalid line: '%s'", info->filename, info->linenr, info->line);
+        } else if (!fnmatch("path_seq", info->key, 0)) {
+            rpl->path_seq = (uint8_t)strtoul(info->value, NULL, 0);
+        } else {
+            WARN("%s:%d: invalid key: '%s'", info->filename, info->linenr, info->line);
+        }
+    }
+    storage_close(info);
+    return true;
+}
+
+void rpl_storage_store(const struct rpl_ctx *rpl)
+{
+    struct storage_parse_info *info;
+
+    info = storage_open_prefix("rpl", "w");
+    if (!info)
+        return;
+
+    fprintf(info->file, "path_seq = %u\n", rpl->path_seq);
+    fflush(info->file);
+    fsync(fileno(info->file));
+    storage_close(info);
+}
---
+++ app_wsrd/ipv6/rpl_storage.h
@@ -0,0 +1,22 @@
+/*
+ * SPDX-License-Identifier: LicenseRef-MSLA
+ * Copyright (c) 2024 Silicon Laboratories Inc. (www.silabs.com)
+ *
+ * The licensor of this software is Silicon Laboratories Inc. Your use of this
+ * software is governed by the terms of the Silicon Labs Master Software License
+ * Agreement (MSLA) available at [1].  This software is distributed to you in
+ * Object Code format and/or Source Code format and is governed by the sections
+ * of the MSLA applicable to Object Code, Source Code and Modified Open Source
+ * Code. By using this software, you agree to the terms of the MSLA.
+ *
+ * [1]: https://www.silabs.com/about-us/legal/master-software-license-agreement
+ */
+#ifndef RPL_STORAGE_H
+#define RPL_STORAGE_H
+
+struct rpl_ctx;
+
+bool rpl_storage_load(struct rpl_ctx *rpl);
+void rpl_storage_store(const struct rpl_ctx *rpl);
+
+#endif
