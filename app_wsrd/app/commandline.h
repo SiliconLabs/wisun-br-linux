e0a5ccbd75881bb56a1efee4b4eb039eafa1372e
Kylian Balan <kylian.balan@silabs.com>
2026-02-02 15:32:24 +0100

    wsrd: introduce custom_allowed_channels
    WI_SUN-5247

--- app_wsrd/app/commandline.h
+++ app_wsrd/app/commandline.h
@@ -53,6 +53,7 @@ struct wsrd_conf {
     int  ws_chan_spacing;
     int  ws_chan_count;
     uint8_t ws_allowed_channels[WS_CHAN_MASK_LEN];
+    uint8_t ws_custom_allowed_channels[WS_CHAN_MASK_LEN];
     int tx_power;
     bool enable_apc;
     struct rcp_csma_cfg csma;
